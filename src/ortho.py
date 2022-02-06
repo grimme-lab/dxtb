16dac9c0460671b2e977b0722eed16f4d84671d6
TCH-MGH <thoelzer@mgh.harvard.edu>
2022-02-06 13:09:45 +0100

    Add orthogonality test

---
+++ src/ortho.py
@@ -0,0 +1,46 @@
+
+
+
+import math
+
+from basis import Cgto_Type
+
+""" Gram-Schmidt orthonormalization routines for contracted Gaussian basis functions """
+
+def orthogonalize(cgtoi, cgtoj):
+    """Orthogonalize a contracted Gaussian basis function to an existing basis function
+
+    Args:
+        cgtoi (Cgto_Type): Existing basis function
+        cgtoj (Cgto_Type): Basis function to orthogonalize
+    """
+
+    if cgtoi.ang != cgtoj.ang:
+        return
+    
+    # cgtoi
+    overlap = 0.0
+    for ipr in range(cgtoi.nprim):
+        for jpr in range(cgtoj.nprim):
+            eab = cgtoi.alpha[ipr] + cgtoj.alpha[jpr]
+            oab = 1.0 / eab
+            kab = math.sqrt(math.pi*oab)**3
+            overlap += cgtoi.coeff[ipr] * cgtoj.coeff[jpr] * kab
+
+    cgtoj.alpha[cgtoj.nprim:cgtoj.nprim+cgtoi.nprim] = cgtoi.alpha[:cgtoi.nprim]
+    cgtoj.coeff[cgtoj.nprim:cgtoj.nprim+cgtoi.nprim] = -overlap*cgtoi.coeff[:cgtoi.nprim]
+    cgtoj.nprim += cgtoi.nprim
+
+    # cgtoj
+    overlap = 0.0
+    for ipr in range(cgtoj.nprim):
+        for jpr in range(cgtoj.nprim):
+            eab = cgtoj.alpha[ipr] + cgtoj.alpha[jpr]
+            oab = 1.0 / eab
+            kab = math.sqrt(math.pi*oab)**3
+            overlap = overlap + cgtoj.coeff[ipr] * cgtoj.coeff[jpr] * kab
+
+
+    cgtoj.coeff[:cgtoj.nprim] = cgtoj.coeff[:cgtoj.nprim] / math.sqrt(overlap)
+
+    return
---
+++ test/test_cgto_ortho.py
@@ -0,0 +1,43 @@
+
+from unittest import TestCase
+import torch
+
+from basis import Cgto_Type
+from slater import slater_to_gauss
+from overlap import overlap_cgto
+from ortho import orthogonalize
+
+""" Testing the orthogonality of orbital overlap. """
+
+class Test_Cgto_Ortho(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        print("Test_Cgto_Ortho")
+        
+    def test_ortho_1s_2s(self):
+        """ Test orthogonality of 1s and 2s orbitals"""
+
+        # orbitals
+        ng, l = 5, 0
+
+        # same site 
+        r2 = 0.0
+        vec = torch.tensor([0., 0., 0.])
+
+        # create gaussians
+        cgtoi, cgtoj = Cgto_Type(), Cgto_Type()
+        slater_to_gauss(5, 1, l, 1.0, cgtoi, True)
+        slater_to_gauss(2, 2, l, 1.0, cgtoj, True)
+
+        orthogonalize(cgtoi, cgtoj)
+
+        # normalised self-overlap
+        overlap = overlap_cgto(cgtoj, cgtoj, r2, vec, 100.0)
+        self.assertTrue(torch.allclose(overlap, torch.eye(1), rtol=1e-05, atol=1e-05, equal_nan=False), msg=f"Self overlap not identity:\n {overlap}")
+       
+        # orthogonal overlap
+        overlap = overlap_cgto(cgtoi, cgtoj, r2, vec, 100.0)
+        self.assertTrue(torch.allclose(overlap, torch.zeros(1), rtol=1e-05, atol=1e-05, equal_nan=False), msg=f"Self overlap not identity:\n {overlap}")
+
+        return
