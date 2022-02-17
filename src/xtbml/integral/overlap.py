Fix asymetric case for overlap calculation

src/xtbml/integral/overlap.py
@@ -286,7 +286,7 @@ def overlap_cgto(cgtoj, cgtoi, r2, vec, intcut):
                     for mli in range(mlao[cgtoi.ang]):
                         for mlj in range(mlao[cgtoj.ang]):
                             val = _overlap_3d(rpj, rpi, lx[:, mlj+lmap[cgtoj.ang]], lx[:, mli+lmap[cgtoi.ang]], s1d)
-                            s3d[mlj, mli] += cc*val
+                            s3d[mli, mlj] += cc*val
 
     with torch.profiler.record_function("transform overlap"):
         # transform overlap matrix

test/test_overlap/test_cgto_ortho.py
@@ -1,4 +1,3 @@
-
 from unittest import TestCase
 import torch
 
@@ -9,21 +8,21 @@ from xtbml.basis.ortho import orthogonalize
 
 """ Testing the orthogonality of orbital overlap. """
 
-class Test_Cgto_Ortho(TestCase):
 
+class Test_Cgto_Ortho(TestCase):
     @classmethod
     def setUpClass(cls):
         print("Test_Cgto_Ortho")
-        
+
     def test_ortho_1s_2s(self):
-        """ Test orthogonality of 1s and 2s orbitals"""
+        """Test orthogonality of 1s and 2s orbitals"""
 
         # orbitals
         ng, l = 5, 0
 
-        # same site 
+        # same site
         r2 = 0.0
-        vec = torch.tensor([0., 0., 0.])
+        vec = torch.tensor([0.0, 0.0, 0.0])
 
         # create gaussians
         cgtoi, cgtoj = Cgto_Type(), Cgto_Type()
@@ -34,10 +33,52 @@ class Test_Cgto_Ortho(TestCase):
 
         # normalised self-overlap
         overlap = overlap_cgto(cgtoj, cgtoj, r2, vec, 100.0)
-        self.assertTrue(torch.allclose(overlap, torch.eye(1), rtol=1e-05, atol=1e-05, equal_nan=False), msg=f"Self overlap not identity:\n {overlap}")
-       
+        self.assertTrue(
+            torch.allclose(
+                overlap, torch.eye(1), rtol=1e-05, atol=1e-05, equal_nan=False
+            ),
+            msg=f"Self overlap not identity:\n {overlap}",
+        )
+
         # orthogonal overlap
         overlap = overlap_cgto(cgtoi, cgtoj, r2, vec, 100.0)
-        self.assertTrue(torch.allclose(overlap, torch.zeros(1), rtol=1e-05, atol=1e-05, equal_nan=False), msg=f"Self overlap not identity:\n {overlap}")
+        self.assertTrue(
+            torch.allclose(
+                overlap, torch.zeros(1), rtol=1e-05, atol=1e-05, equal_nan=False
+            ),
+            msg=f"Self overlap not identity:\n {overlap}",
+        )
 
         return
+
+    def test_overlap_h_c(self):
+        """
+        Compare against reference calculated with tblite-int H C 0,0,1.4 --bohr --method gfn1
+        """
+        from xtbml.param.gfn1 import GFN1_XTB as par
+        from xtbml.xtb.calculator import Basis
+
+        basis = Basis(["H", "C"], par)
+        h = basis.cgto.get("H")
+        c = basis.cgto.get("C")
+
+        vec = torch.tensor([0.0, 0.0, 1.4])
+        r2 = vec.dot(vec)
+
+        ref = [
+            torch.tensor([[+6.77212228e-01]]),
+            torch.tensor([[+0.00000000e-00], [+0.00000000e-00], [-5.15340812e-01]]),
+            torch.tensor([[+7.98499991e-02]]),
+            torch.tensor([[+0.00000000e-00], [+0.00000000e-00], [-1.72674504e-01]]),
+        ]
+
+        torch.set_printoptions(precision=10)
+        for ish in h:
+            for jsh in c:
+                overlap = overlap_cgto(ish, jsh, r2, vec, 100.0)
+                self.assertTrue(
+                    torch.allclose(
+                        overlap, ref.pop(0), rtol=1e-05, atol=1e-05, equal_nan=False
+                    ),
+                    msg=f"Overlap does not match:\n {overlap}",
+                )