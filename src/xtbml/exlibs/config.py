5580c19b154bb298ccb681b9a6e38044c8ea0496
marvinfriede <51965259+marvinfriede@users.noreply.github.com>
2022-04-08 17:27:57 +0200

    Move dimensions to own file

---
+++ src/xtbml/exlibs/config.py
@@ -0,0 +1,5 @@
+dimDipole = 3
+"""Number of dipole components used in tblite library (x, y, z)"""
+
+dimQuadrupole = 6
+"""Number of quadrupole components used in tblite library (xx, xy, yy, xz, yz, zz)"""
