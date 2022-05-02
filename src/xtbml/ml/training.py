7f1af16400029fc22f6960ac2197c55a01d37026
HoelzerC <hoelzer@thch.uni-bonn.de>
2022-05-02 20:28:05 +0200

    Add basic bookkeeping and refactor dataloading

--- src/xtbml/ml/training.py
+++ src/xtbml/ml/training.py
@@ -1,10 +1,13 @@
 """ Simple training pipline for pytoch ML model. """
 
+import datetime
 import torch
-import json
+import torch.nn as nn
+from pathlib import Path
+import pandas as pd
 
 from xtbml.ml.util import load_model_from_cfg
-from xtbml.data.dataset import Sample, Reaction, ReactionDataset
+from xtbml.data.dataset import ReactionDataset
 
 
 def train():
@@ -13,6 +16,16 @@ def train():
     # TODO: load data correctly
     dataset = get_dummy_dataset()
 
+    # TODO: extend padding to number of parameters
+    # prune to constant number of partners
+    if True:
+        np = [len(r.partners) for r in dataset.reactions]
+        idxs = [i for i, x in enumerate(np) if x != 2]
+        for i in reversed(idxs):
+            dataset.rm_reaction(idx=i)
+        cc = [r.uid for r in dataset.reactions]
+        print(f"Dataset contains {len(cc)} reactions: {cc}")
+
     # setup dataloader
     dl = dataset.get_dataloader({"batch_size": 2})
 
@@ -20,9 +33,9 @@ def train():
     cfg_ml = {
         "model_architecture": "Basic_CNN",
         "training_optimizer": "Adam",
-        "training_loss_fn": "MSELoss",
+        "training_loss_fn": "L1Loss",
         "training_lr": 0.01,
-        "epochs": 3,
+        "epochs": 100,
     }
 
     # load components
@@ -30,9 +43,17 @@ def train():
 
     # run training
     model.train()
+    losses = []
+    mads, mads_ref = [], []
     for i in range(cfg_ml["epochs"]):
         print(f"EPOCH {i}")
-        for (batched_samples, batched_reaction) in dl:
+        losses_epoch = []
+        mads_epoch, mads_ref_epoch = [], []
+        for batched_samples, batched_reaction in dl:
+
+            # some samples required in reaction are not available
+            if batched_samples == []:
+                continue
 
             # prediction based on QM features
             y = model(batched_samples, batched_reaction)
@@ -40,7 +61,7 @@ def train():
             # TODO: where to use reaction.egfn1?
 
             # reshape to avoid broadcasting
-            y = y.view(y.size(0), -1)
+            # y = y.view(y.size(0), -1)
             y_true = batched_reaction.eref
 
             # optimize model parameter and feature parameter
@@ -51,38 +72,44 @@ def train():
             # TODO: optimise parameters back to initial positions
             #       (propagate gradients and update parameters)
 
+            # print(f"loss: {loss} | y: {y} | y_true: {y_true}")
+
+            # bookkeeping
+            mad_fn = nn.L1Loss(reduction="mean")
+            losses_epoch.append(loss.item())
+            mads_epoch.append(mad_fn(y, y_true).item())
+            mads_ref_epoch.append(mad_fn(batched_reaction.egfn1, y_true).item())
+
+        # Loss per epoch is average of batch losses
+        losses.append(sum(losses_epoch) / len(losses_epoch))
+        mads.append(sum(mads_epoch) / len(mads_epoch))
+        mads_ref.append(sum(mads_ref_epoch) / len(mads_ref_epoch))
+
     print("Finished training")
+
+    df = pd.DataFrame(
+        list(zip(losses, mads, mads_ref)), columns=["Losses", "MAD", "MAD_ref"]
+    )
+
+    # save model
+    uid = f'{datetime.datetime.now().strftime("%Y%m%d%H%M")}'
+    save_name = f"../models/{uid}"
+    df.to_csv(save_name + "_df.csv", index=True)
+    torch.save(model.state_dict(), save_name + "_model.pt")
 
 
 def get_dummy_dataset() -> ReactionDataset:
     """Return a preliminary dataset for setting up the workflow."""
+
     # load json with real data
-    with open("/home/ch/PhD/projects/xtbML/testfeatures.json", "r") as f:
-        data = json.load(f)
+    path_samples = Path(Path.cwd(), "../data/features.json")
+    path_reactions = Path(Path.cwd(), "../data/reactions.json")
 
-    dummy_samples = []
-    for k, v in data.items():
-        abc = Sample(
-            uid=k,
-            xyz=torch.tensor(v["xyz"]),
-            numbers=torch.tensor(v["numbers"]),
-            egfn1=torch.tensor(v["egfn1"]),
-            ovlp=torch.tensor(v["ovlp"]),
-            h0=torch.tensor(v["h0"]),
-            cn=torch.tensor(v["cn"]),
-        )
-        dummy_samples.append(abc)
-
-    r = Reaction(
-        uid="TEST",
-        partners=["MB16-43/LiH", "MB16-43/42"],
-        nu=torch.tensor([-1, 1]),
-        egfn1=torch.tensor([1.23]),
-        eref=torch.tensor([1.54]),
+    # samples = Samples.from_json(path_samples)
+    # reactions = Reactions.from_json(path_reactions)
+    dataset = ReactionDataset.create_from_disk(
+        path_reactions=path_reactions, path_samples=path_samples
     )
-
-    dummy_reactions = [r, r]
-    dataset = ReactionDataset(samples=dummy_samples, reactions=dummy_reactions)
 
     # apply simple padding
     dataset.pad()
