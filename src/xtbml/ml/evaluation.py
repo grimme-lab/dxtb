6c9be6691f458dc8e4d5a009ff2a6a646117c902
marvinfriede <51965259+marvinfriede@users.noreply.github.com>
2022-05-04 15:20:53 +0200

    Calculate WTMAD-2 for small test set

--- src/xtbml/ml/evaluation.py
+++ src/xtbml/ml/evaluation.py
@@ -21,6 +21,7 @@ def evaluate():
     for i in reversed(idxs):
         dataset.rm_reaction(idx=i)
     cc = [r.uid for r in dataset.reactions]
+    bset = [r.uid.split("_")[0] for r in dataset.reactions]
     print(f"Dataset contains {len(cc)} reactions: {cc}")
 
     # remove all samples with missing values
@@ -55,8 +56,39 @@ def evaluate():
         enn.append(y.item())
         print(f"Enn: {enn[i]:.2f} | Eref: {eref[i]:.2f} | Egfn1: {egfn1[i]:.2f}")
 
-    df = pd.DataFrame(list(zip(eref, egfn1, enn)), columns=["Eref", "Egfn1", "Enn"])
+    df = pd.DataFrame(
+        list(zip(bset, eref, egfn1, enn)), columns=["subset", "Eref", "Egfn1", "Enn"]
+    )
     df["dEgfn1"] = (df["Eref"] - df["Egfn1"]).abs()
     df["dEnn"] = (df["Eref"] - df["Enn"]).abs()
     print(df)
+
     print(df[["dEgfn1", "dEnn"]].describe())
+    print("")
+    print("WTMAD-2: Egfn1", wtmad2(df, "Egfn1", "Eref"))
+    print("")
+    print("WTMAD-2: Enn", wtmad2(df, "Enn", "Eref"))
+
+
+def wtmad2(df, colname_target, colname_ref, set_column="subset"):
+    AVG = 56.84
+    subsets = df.groupby([set_column])
+    subset_names = df[set_column].unique()
+
+    # number of reactions in each subset
+    counts = subsets.count()[colname_target]
+
+    # compute average reaction energy for each subset
+    avg_subsets = subsets.mean()[colname_ref]
+
+    wtmad = 0
+    for i in range(subsets.ngroups):
+        sdf = subsets.get_group(subset_names[i])
+
+        # pandas' mad is not the MAD we usually use, our MAD is actually MUE/MAE
+        # https://github.com/pandas-dev/pandas/blob/v1.4.2/pandas/core/generic.py#L10813
+        mue = (sdf[colname_ref] - sdf[colname_target]).abs().mean()
+        wtmad += counts[i] * AVG / avg_subsets[i] * mue
+        print(f"Subset {subset_names[i]} ({counts[i]} entries): MUE {mue:.3f}")
+
+    return wtmad / len(df.index)
