Correct averages and adapt tests accordingly

src/xtbml/ml/loss.py
@@ -1,104 +1,11 @@
 """ Module for custom loss functions."""
 
-
-import copy
-import os
-from pathlib import Path
 import torch
 import torch.nn.functional as F
-from typing import List, Literal, Optional
+from typing import List, Literal
 
 from ..typing import Tensor
-from ..utils import dict_reorder
 from ..data.dataset import get_gmtkn_dataset
-
-
-def get_gmtkn_ref_values(
-    path: Path = Path(Path.cwd(), "../data/GMTKN55-main"), name: str = ".average"
-) -> dict:
-    """Get reference values for GMTKN55 subset averages.
-
-    Parameters
-    ----------
-    path : Path, optional
-        Path to GMTKN55 folder, by default Path(Path.cwd(), "../data/GMTKN55-main")
-    name : str, optional
-        Name of the file(s) containing the average energy per subset, by default ".average"
-
-    Returns
-    -------
-    dict
-        Dictionary containing the average energy for each subset. Keys sorted alphabetically
-    """
-    d = {}
-    for root, dirs, files in os.walk(path):
-        if name in files:
-            with open(os.path.join(root, name), "r") as file:
-                d[os.path.basename(root)] = torch.tensor(float(file.read()))
-    # no seperate folder
-    d["BH76RC"] = {".numen": torch.tensor(30.0), ".average": torch.tensor(21.39)}.get(
-        name
-    )
-    return dict_reorder(d)
-
-
-gmtkn_ref = {
-    "ACONF": {"count": 15.0, "avg": 1.83},
-    "ADIM6": {"count": 6.0, "avg": 3.36},
-    "AHB21": {"count": 21.0, "avg": 22.49},
-    "AL2X6": {"count": 6.0, "avg": 35.88},
-    "ALK8": {"count": 8.0, "avg": 62.6},
-    "ALKBDE10": {"count": 10.0, "avg": 100.69},
-    "Amino20x4": {"count": 80.0, "avg": 2.44},
-    "BH76": {"count": 76.0, "avg": 18.61},  # "BH76": 19.40 / 106
-    "BH76RC": {"count": 30.0, "avg": 21.39},
-    "BHDIV10": {"count": 10.0, "avg": 45.33},
-    "BHPERI": {"count": 26.0, "avg": 20.87},
-    "BHROT27": {"count": 27.0, "avg": 6.27},
-    "BSR36": {"count": 36.0, "avg": 16.2},
-    "BUT14DIOL": {"count": 64.0, "avg": 2.8},
-    "C60ISO": {"count": 9.0, "avg": 98.25},
-    "CARBHB12": {"count": 12.0, "avg": 6.04},
-    "CDIE20": {"count": 20.0, "avg": 4.06},
-    "CHB6": {"count": 6.0, "avg": 26.79},
-    "DARC": {"count": 14.0, "avg": 32.47},
-    "DC13": {"count": 13.0, "avg": 54.98},
-    "DIPCS10": {"count": 10.0, "avg": 654.26},
-    "FH51": {"count": 51.0, "avg": 31.01},
-    "G21EA": {"count": 25.0, "avg": 33.62},
-    "G21IP": {"count": 36.0, "avg": 257.61},
-    "G2RC": {"count": 25.0, "avg": 51.26},
-    "HAL59": {"count": 59.0, "avg": 4.59},
-    "HEAVY28": {"count": 28.0, "avg": 1.24},
-    "HEAVYSB11": {"count": 11.0, "avg": 58.02},
-    "ICONF": {"count": 17.0, "avg": 3.27},
-    "IDISP": {"count": 6.0, "avg": 14.22},
-    "IL16": {"count": 16.0, "avg": 109.05},
-    "INV24": {"count": 24.0, "avg": 31.85},
-    "ISO34": {"count": 34.0, "avg": 14.57},
-    "ISOL24": {"count": 24.0, "avg": 21.92},
-    "MB16-43": {"count": 43.0, "avg": 414.73},  # 468.39
-    "MCONF": {"count": 51.0, "avg": 4.97},
-    "NBPRC": {"count": 12.0, "avg": 27.71},
-    "PA26": {"count": 26.0, "avg": 189.05},
-    "PArel": {"count": 20.0, "avg": 4.63},
-    "PCONF21": {"count": 18.0, "avg": 1.62},
-    "PNICO23": {"count": 23.0, "avg": 4.27},
-    "PX13": {"count": 13.0, "avg": 33.36},
-    "RC21": {"count": 21.0, "avg": 35.7},
-    "RG18": {"count": 18.0, "avg": 0.58},
-    "RSE43": {"count": 43.0, "avg": 7.6},
-    "S22": {"count": 22.0, "avg": 7.3},
-    "S66": {"count": 66.0, "avg": 5.47},
-    "SCONF": {"count": 17.0, "avg": 4.6},
-    "SIE4x4": {"count": 16.0, "avg": 33.72},
-    "TAUT15": {"count": 15.0, "avg": 3.05},
-    "UPU23": {"count": 23.0, "avg": 5.72},
-    "W4-11": {"count": 140.0, "avg": 306.91},
-    "WATER27": {"count": 27.0, "avg": 81.14},  # 81.17
-    "WCPT18": {"count": 18.0, "avg": 34.99},
-    "YBDE18": {"count": 18.0, "avg": 49.28},
-}
 
 
 class WTMAD2Loss(torch.nn.Module):
@@ -111,7 +18,7 @@ class WTMAD2Loss(torch.nn.Module):
 
     def __init__(
         self,
-        rel_path: Optional[str] = None,
+        rel_path: str,
         reduction: Literal["mean", "sum", "none"] = "mean",
     ) -> None:
         """_summary_
@@ -130,28 +37,38 @@ class WTMAD2Loss(torch.nn.Module):
         """
         super(WTMAD2Loss, self).__init__()
 
-        if rel_path is None:
-            # hardcoded values
-            self.subsets = copy.deepcopy(gmtkn_ref)
-            for k in self.subsets:
-                # number of reactions in each subset
-                self.subsets[k]["count"] = torch.tensor(self.subsets[k]["count"])
-                # average energy for each subset
-                self.subsets[k]["avg"] = torch.tensor(round(self.subsets[k]["avg"], 2))
-            self.total_avg = torch.tensor([56.84])
-        else:
-            # calculate properties dynamically
-            self.rel_path = rel_path
-            self.calc_properties(self.rel_path)
+        # calculate properties dynamically
+        self.rel_path = rel_path
+        self.reduction = reduction
+        self.calc_properties(self.rel_path)
 
-        if reduction == "none":
-            self.reduction = torch.nn.Identity()
-        elif reduction == "mean":
-            self.reduction = torch.mean
-        elif reduction == "sum":
-            self.reduction = torch.sum
+    @property
+    def reduction(self):
+        """Get reduction mode"""
+        return self._reduction
+
+    @reduction.setter
+    def reduction(self, mode: str):
+        """Set mode for reduction of batch-wise loss to single value.
+
+        Parameters
+        ----------
+        mode : str
+            Reduction mode
+
+        Raises
+        ------
+        TypeError
+            Invalid loss given
+        """
+        if mode == "none":
+            self._reduction = torch.nn.Identity()
+        elif mode == "mean":
+            self._reduction = torch.mean
+        elif mode == "sum":
+            self._reduction = torch.sum
         else:
-            raise TypeError
+            raise TypeError(f"Reduction mode '{mode}' unknown.")
 
     def forward(
         self, input: Tensor, target: Tensor, label: List[str], n_partner: Tensor
@@ -204,26 +121,29 @@ class WTMAD2Loss(torch.nn.Module):
         # load data
         dataset = get_gmtkn_dataset(rel_path)
         d = {}
-        total_avg = torch.Tensor([0])
 
         # collect values
         for r in dataset.reactions:
             subset = r.uid.split("_")[0]
             if subset not in d.keys():
                 d[subset] = {
-                    "ref": torch.Tensor([0]),
-                    "count": torch.Tensor([0]),
+                    "ref": torch.tensor(0.0),
+                    "count": torch.tensor(0.0),
                 }
             d[subset]["ref"] += r.eref.abs()
-            d[subset]["count"] += 1
-            total_avg += r.eref.abs()
+            d[subset]["count"] += torch.tensor(1.0)
 
         # update self
         self.subsets = {
             subset: {
-                "count": d[subset]["count"],
                 "avg": d[subset]["ref"] / d[subset]["count"],
+                "count": d[subset]["count"],
             }
             for subset in d
         }
-        self.total_avg = total_avg / len(dataset)
+
+        # average of all subset averages
+        self.total_avg = torch.tensor(0.0)
+        for _, subset in self.subsets.items():
+            self.total_avg += subset["avg"]
+        self.total_avg /= len(self.subsets)

test/test_ml/test_loss.py
@@ -1,20 +1,28 @@
+from pathlib import Path
 import pytest
 import torch
 
-from xtbml.ml.loss import WTMAD2Loss, get_gmtkn_ref_values
+from xtbml.ml.loss import WTMAD2Loss
 from xtbml.data.dataset import ReactionDataset, get_gmtkn_dataset
 
+from .gmtkn55 import GMTKN55
 
-class Test_WTMAD2Loss:
+
+class TestWTMAD2Loss:
     """Testing the loss calulation based on GMTKN55 weighting."""
+
+    path = "./data"
 
     def setup_class(self):
         print()
         print("Test custom loss function")
-        self.dataset = get_gmtkn_dataset("./data")
+        self.dataset = get_gmtkn_dataset(self.path)
 
-        # subsets in GMTKN-55
+        # subsets in GMTKN55
         self.all_sets = set([r.uid.split("_")[0] for r in self.dataset.reactions])
+
+        # loss function
+        self.loss_fn = WTMAD2Loss(self.path)
 
     def teardown_class(self):
         # teardown_class called once for the class
@@ -47,49 +55,40 @@ class Test_WTMAD2Loss:
             partners = [s.split("/")[0] for s in r.partners]
             assert {subset} == set(partners), "Partner and reaction naming inconsistent"
 
-    @pytest.mark.parametrize("dynamic_loading", [False, True])
-    def test_loading(self, dynamic_loading):
+    def test_loading(self):
+        """Check consistency of GMTKN55 through by comparing with hard-coded averages and counts (number of reactions) of subsets."""
         atol = 1.0e-2
         rtol = 1.0e-4
+        TOTAL_AVG = 57.82
 
-        if dynamic_loading:
-            # load GMTKN-55 dynamically from disk
-            loss_fn = WTMAD2Loss(rel_path="./data")
+        for (_, target), (_, ref) in zip(self.loss_fn.subsets.items(), GMTKN55.items()):
+            # counts (type and value)
+            assert ref["count"].is_integer()
+            assert target["count"].item().is_integer()
+            assert int(ref["count"]) == int(target["count"])
 
-            # TODO - fix bug for those
-            loss_fn.subsets["MB16-43"]["avg"] = torch.tensor(414.73)
-            loss_fn.subsets["WATER27"]["avg"] = torch.tensor(81.14)
-            loss_fn.total_avg = 56.84
-        else:
-            loss_fn = WTMAD2Loss()
-
-        def base_test(subsets: dict, gmtkn_ref: dict, key: str):
-            for k, v in subsets.items():
-                assert torch.allclose(
-                    v[key],
-                    gmtkn_ref[k],
-                    rtol=rtol,
-                    atol=atol,
-                    equal_nan=False,
-                )
-
-        # averages
-        avgs = get_gmtkn_ref_values(path="./data/GMTKN55-main")
-        base_test(loss_fn.subsets, avgs, "avg")
-
-        # counts
-        counts = get_gmtkn_ref_values(path="./data/GMTKN55-main", name=".numen")
-        base_test(loss_fn.subsets, counts, "count")
+            # averages
+            assert torch.allclose(
+                torch.tensor(ref["avg"]),
+                target["avg"],
+                rtol=rtol,
+                atol=atol,
+                equal_nan=False,
+            )
 
         # total average
-        assert loss_fn.total_avg == 56.84
+        assert torch.allclose(
+            self.loss_fn.total_avg,
+            torch.tensor(TOTAL_AVG),
+            rtol=rtol,
+            atol=atol,
+            equal_nan=False,
+        )
 
         # total len
-        assert len(loss_fn.subsets) == 55
+        assert len(self.loss_fn.subsets) == 55
 
     def test_single(self):
-        loss_fn = WTMAD2Loss()
-
         n_reactions = 2.0
         n_reactions_i = 2
         input = torch.arange(n_reactions, requires_grad=True)
@@ -99,10 +98,11 @@ class Test_WTMAD2Loss:
         # reaction lengths
         n_partner = torch.tensor([n_reactions_i, n_reactions_i])
 
-        output = loss_fn(input, target, label, n_partner)
+        self.loss_fn.reduction = "mean"
+        output = self.loss_fn(input, target, label, n_partner)
 
         assert output.shape == torch.Size([])
-        assert output.item() == 25.41281509399414
+        assert output.item() == 25.791534423828125
 
     @pytest.mark.grad
     def test_grad(self):
@@ -117,7 +117,7 @@ class Test_WTMAD2Loss:
         n_partner = torch.tensor([n_reactions_i, n_reactions_i])
 
         assert torch.autograd.gradcheck(
-            WTMAD2Loss(),
+            WTMAD2Loss(self.path),
             (input, target, label, n_partner),
             raise_exception=True,
         )
@@ -126,25 +126,25 @@ class Test_WTMAD2Loss:
     def test_gmtkn(self, batchsize):
         # TODO: compare subset and total WTMAD with paper and with implementation in evaluation.py
 
-        print("load dl with bs", batchsize)
+        print("\nload dl with bs", batchsize)
         dl = self.dataset.get_dataloader({"batch_size": batchsize, "shuffle": True})
-        loss_fn = WTMAD2Loss(reduction="sum")
+        self.loss_fn.reduction = "sum"
 
         loss = torch.tensor([0.0])
-        for i, (batched_samples, batched_reaction) in enumerate(dl):
+        for i, (_, batched_reaction) in enumerate(dl):
 
             # derive subset from partner list
             subsets = [s.split("/")[0] for s in batched_reaction.partners]
             # different number of partners per reaction
             n_partner = torch.count_nonzero(batched_reaction.nu, dim=1)
 
-            loss += loss_fn(
+            loss += self.loss_fn(
                 batched_reaction.egfn1, batched_reaction.eref, subsets, n_partner
             )
 
         assert torch.allclose(
             loss,
-            torch.tensor([44055.55078125]),
+            torch.tensor([44728.2891]),
             rtol=1.0e-4,
             atol=1.0e-6,
             equal_nan=False,
@@ -153,7 +153,7 @@ class Test_WTMAD2Loss:
     def test_gmtkn_subsets(self):
 
         dl = self.dataset.get_dataloader({"batch_size": 2, "shuffle": False})
-        loss_fn = WTMAD2Loss(reduction="none")
+        self.loss_fn.reduction = "none"
 
         losses = {k: torch.tensor([0.0]) for k in self.all_sets}
 
@@ -164,7 +164,7 @@ class Test_WTMAD2Loss:
             # different number of partners per reaction
             n_partner = torch.count_nonzero(batched_reaction.nu, dim=1)
 
-            loss = loss_fn(
+            loss = self.loss_fn(
                 batched_reaction.egfn1, batched_reaction.eref, subsets, n_partner
             )
 
@@ -176,7 +176,7 @@ class Test_WTMAD2Loss:
 
         # normalise loss per subset (optional)
         for k, v in losses.items():
-            losses[k] = v * len(self.dataset) / loss_fn.total_avg
+            losses[k] = v * len(self.dataset) / self.loss_fn.total_avg
 
         # print(losses)
 