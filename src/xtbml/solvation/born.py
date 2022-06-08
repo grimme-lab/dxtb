59192fabf88b0fcc3b7e14ff4fc1196975067562
marvinfriede <51965259+marvinfriede@users.noreply.github.com>
2022-06-08 18:00:19 +0200

    Vectorize with batch support

--- src/xtbml/solvation/born.py
+++ src/xtbml/solvation/born.py
@@ -1,7 +1,42 @@
-"""Integrator for Born radii based on the Onufriev-Bashford-Case model."""
+"""
+Born Radii
+==========
+
+Integrator for Born radii based on the Onufriev-Bashford-Case model.
+
+Example
+-------
+
+>>> import torch
+>>> from xtbml.solvation import born
+>>> numbers = torch.tensor([14, 1, 1, 1, 1])
+>>> positions = torch.tensor(
+...     [
+...         0.00000000000000,
+...         -0.00000000000000,
+...         0.00000000000000,
+...         1.61768389755830,
+...         1.61768389755830,
+...         -1.61768389755830,
+...         -1.61768389755830,
+...         -1.61768389755830,
+...         -1.61768389755830,
+...         1.61768389755830,
+...         -1.61768389755830,
+...         1.61768389755830,
+...         -1.61768389755830,
+...         1.61768389755830,
+...         1.61768389755830,
+...     ],
+... ).reshape((-1, 3))
+>>> rads = born.get_born_radii(numbers, positions)
+>>> print(rads)
+tensor([3.6647, 2.4621, 2.4621, 2.4621, 2.4621])
+"""
 
 import torch
 from typing import Optional
+
 from ..typing import Tensor
 from .data import vdw_rad_d3
 
@@ -18,7 +53,7 @@ def get_born_radii(
 ) -> Tensor:
     """
     Calculate Born radii for a set of atoms using the Onufriev-Bashford-Case model published in
-    - A. Onufriev, D. Bashford, D. A. Case, *Proteins: Struct., Funct., Bioinf.*, **2004**, 55, 383–394. DOI: `10.1002/prot.20033 < https://doi.org/10.1002/prot.20033>`__
+    - A. Onufriev, D. Bashford, D. A. Case, *Proteins: Struct., Funct., Bioinf.*, **2004**, 55, 383–394. DOI: `10.1002/prot.20033 <https://doi.org/10.1002/prot.20033>`__
 
     Args:
         numbers: Atomic numbers of the atoms
@@ -34,8 +69,6 @@ def get_born_radii(
         Born radii for the atoms
     """
 
-    if cutoff is None:
-        cutoff = torch.tensor(25.0, dtype=positions.dtype)
     if rvdw is None:
         rvdw = vdw_rad_d3[numbers].type(positions.dtype)
     if numbers.shape != rvdw.shape:
@@ -45,26 +78,96 @@ def get_born_radii(
     if numbers.shape != positions.shape[:-1]:
         raise ValueError("Shape of positions is not consistent with atomic numbers")
 
+    # mask for padding
+    mask = numbers != 0
+    zero = torch.tensor(0.0, dtype=positions.dtype)
+
+    # get dielectric descreening integral I for Eq.6 (psi = I * scaled_rho)
+    # NOTE: compute_psi actually only computes I not psi
+    psi = compute_psi(numbers, positions, rvdw, cutoff, descreening)
+
+    # some temporary variables
     srvdw = rvdw - born_offset
+    r1 = torch.where(mask, 1.0 / rvdw, zero)
+    s1 = torch.where(mask, 1.0 / srvdw, zero)
+    psis2 = 0.5 * psi * srvdw
 
-    psi = compute_psi(numbers, positions, rvdw, descreening)
+    # Eq.6 part 1: tanh(alpha*psi - beta*psi^2 + gamma*psi^3)
+    tmp2 = psis2 * (obc[2] * psis2 - obc[1])
+    tmp = psis2 * (obc[0] + tmp2)
+    th = torch.tanh(tmp)
 
-    r1 = torch.divide(torch.tensor(1.0), rvdw)
-    s1 = torch.divide(torch.tensor(1.0), srvdw)
-
-    psis2 = psi * 0.5 * srvdw
-
-    # Eq.6: tanh(alpha*psi - beta*psi^2 + gamma*psi^3)
-    arg2 = psis2 * (obc[2] * psis2 - obc[1])
-    arg = psis2 * (obc[0] + arg2)
-    th = torch.tanh(arg)
-
-    brads = torch.divide(torch.tensor(1.0), (s1 - r1 * th)) * born_scale
-
-    return brads
+    # Eq.6 part 2: R^-1 = scaled_rho^-1 - rho^-1 * tanh(arg)
+    return torch.where(mask, born_scale / (s1 - r1 * th), zero)
 
 
 def compute_psi(
+    numbers: Tensor,
+    positions: Tensor,
+    rvdw: Tensor,
+    cutoff: Tensor = torch.tensor(66.0),
+    descreening: float = 0.8,
+) -> Tensor:
+
+    # TODO: implement individual descreening
+    rho = rvdw * descreening
+
+    # mask for padding
+    real = numbers != 0
+    mask = real.unsqueeze(-2) * real.unsqueeze(-1)
+    mask.diagonal(dim1=-2, dim2=-1).fill_(False)
+
+    eps = torch.tensor(torch.finfo(positions.dtype).eps, dtype=positions.dtype)
+    zero = torch.tensor(0.0, dtype=positions.dtype)
+
+    distances = torch.where(
+        mask,
+        torch.cdist(positions, positions, p=2),
+        eps,
+    )
+    r1 = 1.0 / distances
+
+    # mask determining overlapping atoms
+    mask_ovlp = distances < (rvdw.unsqueeze(-2) + rho.unsqueeze(-1))
+
+    # mask for contributions from overlapping atoms
+    # (emulates "(rij + rho_j) > rvdw_i" and "(rij + rho_i) > rvdw_j")
+    mask_ovlp_contr = (distances + rho.unsqueeze(-1)) > (
+        rvdw.unsqueeze(-2) * rvdw.new_ones(rvdw.shape).unsqueeze(-1)
+    )
+
+    # temporary variables
+    d_pl = distances + rho.unsqueeze(-1)
+    d_mi = distances - rho.unsqueeze(-1)
+    d_pr = torch.where(mask, d_pl * d_mi, zero)
+    d_qu = torch.where(mask, d_mi / d_pl, zero)
+
+    # contributions from non-overlapping atoms
+    rho_dpr = torch.where(mask, rho.unsqueeze(-1) / d_pr, zero)
+    ln_dqu = torch.where(mask, 0.5 * torch.log(d_qu) * r1, zero)
+    non_ovlp = rho_dpr + ln_dqu
+
+    # contributions from overlapping atoms
+    rvdw1 = torch.where(mask, 1.0 / rvdw.unsqueeze(-2), zero)
+    d_pl1 = 1.0 / d_pl
+    dpl_rvdw1 = d_pl * rvdw1
+    ln_dpl_rvdw1 = torch.where(mask, torch.log(dpl_rvdw1), zero)
+    ovlp = (
+        rvdw1
+        - d_pl1
+        + 0.5 * r1 * (0.5 * d_mi * (d_pl1 - rvdw1 * dpl_rvdw1) - ln_dpl_rvdw1)
+    )
+
+    psi = torch.where(
+        mask * (distances <= cutoff),
+        torch.where(mask_ovlp * mask_ovlp_contr, ovlp, non_ovlp),
+        zero,
+    )
+
+    return torch.sum(psi, dim=-2)
+
+
+def compute_psi_loops(
     numbers: Tensor,
     positions: Tensor,
     rvdw: Optional[Tensor] = None,
@@ -79,17 +182,15 @@ def compute_psi(
     psi = torch.zeros_like(rvdw)
     for i in range(len(numbers)):
         for j in range(i):
-            if i == j:
-                continue
             vec = positions[i, :] - positions[j, :]
             rij = torch.norm(vec, dim=-1)
+            r1 = 1.0 / rij
+            r12 = 0.5 * r1
 
             rho_i = rho[i]
             rho_j = rho[j]
             rvdw_i = rvdw[i]
             rvdw_j = rvdw[j]
-
-            r1 = 1.0 / rij
 
             # ij contribution
             ap_ij = rij + rho_j
@@ -107,6 +208,17 @@ def compute_psi(
             lnab_ji = 0.5 * torch.log(am_ji / ap_ji) * r1
             g_ji = rhab_ji + lnab_ji
 
+            #
+            rh1_ij = 1.0 / rvdw_i
+            rhr1_ij = 1.0 / ap_ij
+            aprh1_ij = ap_ij * rh1_ij
+            lnab_ij = torch.log(aprh1_ij)
+
+            rh1_ji = 1.0 / rvdw_j
+            rhr1_ji = 1.0 / ap_ji
+            aprh1_ji = ap_ji * rh1_ji
+            lnab_ji = torch.log(aprh1_ji)
+
             # overlap?
             ijov = (rij < (rvdw_i + rho_j)).item()
             jiov = (rij < (rvdw_j + rho_i)).item()
@@ -114,20 +226,20 @@ def compute_psi(
             if not ijov and not jiov:  # nonoverlaping spheres
                 # ij contribution
                 psi[i] = psi[i] + g_ij
+                psi[j] = psi[j] + g_ji
 
                 # ji contribution
                 if torch.abs(rho_i - rho_j) < 1e-6:  # equal reduced radii
-                    psi[j] = psi[j] + g_ji
-
+                    pass
                 else:  # unequal reduced radii
-                    psi[j] = psi[j] + g_ji
+                    pass
 
             elif not ijov and jiov:
                 # ij contribution
                 psi[i] = psi[i] + g_ij
 
                 # ji contribution
-                if rij + rho_i > rvdw_j:
+                if (rij + rho_i) > rvdw_j:
                     r12 = 0.5 * r1
                     rh1 = 1.0 / rvdw_j
                     rhr1 = 1.0 / ap_ji
@@ -167,15 +279,15 @@ def compute_psi(
                 # ij contribution
                 if rij + rho_j > rvdw_i:
                     r12 = 0.5 * r1
-                    rh1 = 1.0 / rvdw_i
-                    rhr1 = 1.0 / ap_ij
-                    aprh1_ij = ap_ij * rh1
+                    rh1_ij = 1.0 / rvdw_i
+                    rhr1_ij = 1.0 / ap_ij
+                    aprh1_ij = ap_ij * rh1_ij
                     lnab_ij = torch.log(aprh1_ij)
 
                     g_ij = (
-                        rh1
-                        - rhr1
-                        + r12 * (0.5 * am_ij * (rhr1 - rh1 * aprh1_ij) - lnab_ij)
+                        rh1_ij
+                        - rhr1_ij
+                        + r12 * (0.5 * am_ij * (rhr1_ij - rh1_ij * aprh1_ij) - lnab_ij)
                     )
 
                     psi[i] = psi[i] + g_ij
@@ -183,15 +295,15 @@ def compute_psi(
                 # ji contribution
                 if rij + rho_i > rvdw_j:
                     r12 = 0.5 * r1
-                    rh1 = 1.0 / rvdw_j
-                    rhr1 = 1.0 / ap_ji
-                    aprh1_ji = ap_ji * rh1
+                    rh1_ji = 1.0 / rvdw_j
+                    rhr1_ji = 1.0 / ap_ji
+                    aprh1_ji = ap_ji * rh1_ji
                     lnab_ji = torch.log(aprh1_ji)
 
                     g_ji = (
-                        rh1
-                        - rhr1
-                        + r12 * (0.5 * am_ji * (rhr1 - rh1 * aprh1_ji) - lnab_ji)
+                        rh1_ji
+                        - rhr1_ji
+                        + r12 * (0.5 * am_ji * (rhr1_ji - rh1_ji * aprh1_ji) - lnab_ji)
                     )
 
                     psi[j] = psi[j] + g_ji
--- test/test_solvation/test_born.py
+++ test/test_solvation/test_born.py
@@ -1,7 +1,10 @@
 import pytest
 import torch
+from xtbml.exlibs.tbmalt import batch
 
 from xtbml.solvation import born
+from xtbml.solvation.data import vdw_rad_d3
+
 
 from .samples import mb16_43
 
@@ -13,20 +16,19 @@ class TestBorn:
     def setup_class(cls):
         print(cls.__name__)
 
-    @pytest.mark.parametrize("dtype", [torch.float32])
+    @pytest.mark.parametrize("dtype", [torch.float32, torch.float64])
     def test_psi_mb1643_01(self, dtype: torch.dtype):
         """Test Born psi for mb16_43_01."""
         sample = mb16_43["01"]
 
         numbers = sample["numbers"]
         positions = sample["positions"].type(dtype)
+        rvdw = vdw_rad_d3[numbers].type(dtype)
 
-        psi = born.compute_psi(numbers, positions)
-        print("psi", psi)
+        psi = born.compute_psi(numbers, positions, rvdw)
+        assert torch.allclose(psi, sample["psi"].type(dtype))
 
-        assert torch.allclose(psi, sample["psi"].type(dtype), atol=1e-4)
-
-    @pytest.mark.parametrize("dtype", [torch.float32])
+    @pytest.mark.parametrize("dtype", [torch.float32, torch.float64])
     def test_radii_mb1643_01(self, dtype: torch.dtype):
         """Test Born radii for mb16_43_01."""
         sample = mb16_43["01"]
@@ -35,8 +37,84 @@ class TestBorn:
         positions = sample["positions"].type(dtype)
 
         rads = born.get_born_radii(numbers, positions)
+        assert torch.allclose(rads, sample["born"].type(dtype))
 
-        print("rads", rads)
-        print(sample["born"].type(dtype))
+    @pytest.mark.parametrize("dtype", [torch.float32, torch.float64])
+    def test_psi_mb1643_02(self, dtype: torch.dtype):
+        """Test Born psi for mb16_43_02."""
+        sample = mb16_43["02"]
 
-        assert torch.allclose(rads, sample["born"].type(dtype), atol=1e-4)
+        numbers = sample["numbers"]
+        positions = sample["positions"].type(dtype)
+        rvdw = vdw_rad_d3[numbers].type(dtype)
+
+        psi = born.compute_psi(numbers, positions, rvdw)
+        assert torch.allclose(psi, sample["psi"].type(dtype))
+
+    @pytest.mark.parametrize("dtype", [torch.float32, torch.float64])
+    def test_radii_mb1643_02(self, dtype: torch.dtype):
+        """Test Born radii for mb16_43_02."""
+        sample = mb16_43["02"]
+
+        numbers = sample["numbers"]
+        positions = sample["positions"].type(dtype)
+
+        rads = born.get_born_radii(numbers, positions)
+        assert torch.allclose(rads, sample["born"].type(dtype))
+
+    @pytest.mark.parametrize("dtype", [torch.float32, torch.float64])
+    def test_psi_batch(self, dtype: torch.dtype):
+        """Test psi for batch."""
+        sample1, sample2 = mb16_43["01"], mb16_43["SiH4"]
+
+        numbers = batch.pack(
+            (
+                sample1["numbers"],
+                sample2["numbers"],
+            )
+        )
+        positions = batch.pack(
+            (
+                sample1["positions"].type(dtype),
+                sample2["positions"].type(dtype),
+            )
+        )
+        rvdw = vdw_rad_d3[numbers].type(dtype)
+
+        ref = batch.pack(
+            (
+                sample1["psi"].type(dtype),
+                sample2["psi"].type(dtype),
+            ),
+        )
+
+        psi = born.compute_psi(numbers, positions, rvdw)
+        assert torch.allclose(psi, ref)
+
+    @pytest.mark.parametrize("dtype", [torch.float32, torch.float64])
+    def test_radii_batch(self, dtype: torch.dtype):
+        """Test Born radii for batch."""
+        sample1, sample2 = mb16_43["01"], mb16_43["SiH4"]
+
+        numbers = batch.pack(
+            (
+                sample1["numbers"],
+                sample2["numbers"],
+            )
+        )
+        positions = batch.pack(
+            (
+                sample1["positions"].type(dtype),
+                sample2["positions"].type(dtype),
+            )
+        )
+
+        ref = batch.pack(
+            (
+                sample1["born"].type(dtype),
+                sample2["born"].type(dtype),
+            ),
+        )
+
+        rads = born.get_born_radii(numbers, positions)
+        assert torch.allclose(rads, ref)
