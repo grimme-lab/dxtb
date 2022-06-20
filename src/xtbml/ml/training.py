e32b15a3c2601ed0964277f40ee5cc928cf4d911
HoelzerC <hoelzer@thch.uni-bonn.de>
2022-06-20 17:29:27 +0200

    Change notes

--- src/xtbml/ml/training.py
+++ src/xtbml/ml/training.py
@@ -1,8 +1,9 @@
 """ Simple training pipline for pytoch ML model. """
 
+
 import datetime
 import imp
-from typing import Any, Dict, Tuple
+from typing import Any, Dict, Tuple, Union, List
 import torch
 import torch.nn as nn
 import pandas as pd
@@ -11,6 +12,7 @@ from pathlib import Path
 from ..ml.util import load_model_from_cfg
 from ..data.dataset import get_gmtkn_dataset
 from .norm import Normalisation
+from .loss import WTMAD2Loss
 
 
 def train():
@@ -20,27 +22,24 @@ def train():
     root = Path(__file__).resolve().parents[3]
     dataset = get_gmtkn_dataset(Path(root, "data"))
 
-    # TODO: maybe wrap this into a transforms module
-    #   (i.e. module that act on dataset objects)
-    Normalisation.normalise(dataset)
+    # TODO: add feature normalisation
 
     # setup dataloader
     dl = dataset.get_dataloader({"batch_size": 2, "shuffle": True})
 
     # set config for ML
     cfg_ml = {
-        "model_architecture": "Basic_CNN",  # "Basic_CNN" "EGNN"
+        "model_architecture": "Basic_CNN",  # "Basic_CNN" "EGNN" "Simple_Net"
         "training_optimizer": "Adam",
         "training_scheduler": "CyclicLR",
         "training_loss_fn": "WTMAD2Loss",  # "L1Loss",
-        "training_lr": 0.01,  # 0.0001 0.001 0.01
+        "training_loss_fn_path": Path(root, "data"),
+        "training_lr": 0.01,
         "epochs": 60,
     }
 
     # load components
-    model, optimizer, loss_fn, scheduler = load_model_from_cfg(
-        Path(root, "data"), cfg_ml, load_state=False
-    )
+    model, optimizer, loss_fn, scheduler = load_model_from_cfg(cfg_ml, load_state=False)
 
     # run training
     model.train()
@@ -67,7 +66,7 @@ def train():
             y_true = batched_reaction.eref
 
             # optimize model parameter and feature parameter
-            if cfg_ml["training_loss_fn"] == "WTMAD2Loss":
+            if isinstance(loss_fn, WTMAD2Loss):
                 # derive subset from partner list
                 subsets = [s.split("/")[0] for s in batched_reaction.partners]
                 # different number of partners per reaction
@@ -91,9 +90,14 @@ def train():
             mads_epoch.append(mad_fn(y, y_true).item())
             mads_ref_epoch.append(mad_fn(batched_reaction.egfn1, y_true).item())
 
-            if cfg_ml["training_loss_fn"] == "WTMAD2Loss":
+            if isinstance(loss_fn, WTMAD2Loss):
                 wtm2_ref_epoch.append(
-                    loss_fn(batched_reaction.egfn1, y_true, subsets, n_partner).item()
+                    loss_fn(
+                        batched_reaction.egfn1,
+                        y_true,
+                        subsets,
+                        n_partner,
+                    ).item()
                 )
 
         # adapting learning rate epoch-wise
@@ -104,9 +108,13 @@ def train():
         mads.append(sum(mads_epoch) / len(mads_epoch))
         mads_ref.append(sum(mads_ref_epoch) / len(mads_ref_epoch))
         wtm2_ref.append(sum(wtm2_ref_epoch) / len(wtm2_ref_epoch))
-        print(
-            f"Loss: {losses[-1]} | MAD: {mads[-1]} | MAD_REF: {mads_ref[-1]} | WTM2_REF: {wtm2_ref[-1]}"
-        )
+        if isinstance(loss_fn, WTMAD2Loss):
+            wtm2_ref.append(sum(wtm2_ref_epoch) / len(wtm2_ref_epoch))
+            print(
+                f"Loss: {losses[-1]} | MAD: {mads[-1]} | MAD_REF: {mads_ref[-1]} | WTM2_REF: {wtm2_ref[-1]}"
+            )
+        else:
+            print(f"Loss: {losses[-1]} | MAD: {mads[-1]} | MAD_REF: {mads_ref[-1]}")
 
     print("Minimal Loss:", min(losses))
 
