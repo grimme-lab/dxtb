f7c104aba85100dc586d9fa9ca99004ebef01eae
marvinfriede <51965259+marvinfriede@users.noreply.github.com>
2022-07-04 16:05:37 +0200

    Remove old `get_energy` and adapt docs

--- src/xtbml/coulomb/secondorder.py
+++ src/xtbml/coulomb/secondorder.py
@@ -29,7 +29,9 @@ Example
 >>> gexp = torch.tensor(GFN1_XTB.charge.effective.gexp)
 >>> hubbard = get_element_param(GFN1_XTB.element, "gam")
 >>> # calculate energy
->>> e = es2.get_energy(numbers, positions, q, hubbard, average, gexp)
+>>> es = es2.ES2(hubbard=hubbard, average=average, gexp=gexp)
+>>> cache = es.get_cache(numbers, positions)
+>>> e = es.get_energy(cache, qat)
 >>> torch.set_printoptions(precision=7)
 >>> print(torch.sum(e, dim=-1))
 tensor(0.0005078)
@@ -134,7 +136,7 @@ class ES2:
         # Eq.26: Coulomb matrix
         return 1.0 / torch.pow(dist_gexp + torch.pow(avg, -self.gexp), 1.0 / self.gexp)
 
-    def get_energy(self, cache, q):
+    def get_energy(self, cache, q: Tensor):
         """
         Calculate the second-order Coulomb interaction energy.
 
@@ -165,78 +167,21 @@ class ES2:
 
         return mv * q
 
+    def get_potential(self, cache, q: Tensor) -> Tensor:
+        """Calculate the second-order Coulomb interaction potential.
+        
+        Parameters
+        ----------
+        cache : Cache
+            Reusable cache for the Coulomb matrix.
+        q : Tensor
+            Atomic or shell-resolved charges of all atoms.
 
-def get_energy(
-    numbers: Tensor,
-    positions: Tensor,
-    q: Tensor,
-    hubbard: Tensor,
-    lhubbard: dict[int, list[float]] | None = None,
-    average: AveragingFunction = harmonic_average,
-    gexp: Tensor = torch.tensor(2.0),
-) -> Tensor:
-    """
-    Calculate the second-order Coulomb interaction.
-
-    Implements Eq.25 of the following paper:
-    - C. Bannwarth, E. Caldeweyher, S. Ehlert, A. Hansen, P. Pracht, J. Seibert,
-    S. Spicher and S. Grimme, *WIREs Computational Molecular Science*, **2020**, 11, e1493. DOI: `10.1002/wcms.1493 <https://wires.onlinelibrary.wiley.com/doi/10.1002/wcms.1493>`__
-
-    Parameters
-    ----------
-    numbers : Tensor
-        Atomic numbers of the atoms.
-    positions : Tensor
-        Cartesian coordinates of all atoms in the system.
-    q : Tensor
-        Atomic or shell-resolved charges of all atoms.
-    hubbard : Tensor
-        Hubbard parameters of all elements.
-    lhubbard : dict[int, list[float]] | None
-        Shell-resolved scaling factors for Hubbard parameters. If None, no shell resolution is used.
-    average : AveragingFunction
-        Function to use for averaging the Hubbard parameters (default: harmonic_average).
-    gexp : Tensor
-        Exponent of the second-order Coulomb interaction (default: 2.0).
-
-    Returns
-    -------
-    Tensor
-        Atomwise second-order Coulomb interaction energies.
-    """
-
-    h = hubbard[numbers]
-
-    if lhubbard is not None:
-        # NOTE: Maybe use own function here and not "abusing" the IndexHelper?
-        ihelp = IndexHelper.from_numbers(numbers, lhubbard, dtype=positions.dtype)
-        shell_idxs = ihelp.shells_to_atom.type(torch.long)
-
-        h = batch.index(h, shell_idxs) * ihelp.angular
-        positions = batch.index(positions, shell_idxs)
-
-    # masks
-    real = h != 0
-    mask = real.unsqueeze(-2) * real.unsqueeze(-1)
-    mask.diagonal(dim1=-2, dim2=-1).fill_(False)
-
-    # all distances to the power of "gexp" (R^2_AB from Eq.26)
-    dist_gexp = torch.where(
-        mask,
-        torch.pow(torch.cdist(positions, positions, p=2), gexp),
-        torch.tensor(torch.finfo(positions.dtype).eps, dtype=positions.dtype),
-    )
-
-    # Eq.30: averaging function for hardnesses (Hubbard parameter)
-    avg = average(h)
-
-    # Eq.26: Coulomb matrix
-    mat = 1.0 / torch.pow(dist_gexp + torch.pow(avg, -gexp), 1.0 / gexp)
-
-    # Eq.25: single and batched matrix-vector multiplication
-    mv = 0.5 * torch.einsum("...ik, ...k -> ...i", mat, q)
-
-    if lhubbard is not None:
-        return torch.scatter_reduce(mv * q, -1, ihelp.shells_to_atom, reduce="sum")
-
-    return mv * q
+        Returns
+        -------
+        Tensor
+            Atomwise second-order Coulomb interaction potential.
+        """
+        
+        return torch.einsum("...ik, ...k -> ...i", cache.mat, q)
+        
