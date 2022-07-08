c8fe8263f9abd3075fc31ea1b9a09f99b0e9c830
marvinfriede <51965259+marvinfriede@users.noreply.github.com>
2022-07-08 20:25:58 +0200

    Raise exception for non-numeric values

--- src/xtbml/param/util.py
+++ src/xtbml/param/util.py
@@ -19,20 +19,30 @@ def get_element_param(par_element: dict[str, Element], key: str) -> Tensor:
     -------
     Tensor
         Parametrization of all elements (with 0 index being a dummy to allow indexing by atomic numbers).
+
+    Raises
+    ------
+    ValueError
+        If the type of the value of `key` is neither `float` nor `int`.
     """
 
     # dummy for indexing with atomic numbers
     t = [0.0]
 
     for item in par_element.values():
-        t.append(getattr(item, key))
+        val = getattr(item, key)
+        if not isinstance(val, float) and not isinstance(val, int):
+            raise ValueError(f"The key '{key}' contains the non-numeric value '{val}'.")
+
+        t.append(val)
 
     return torch.tensor(t)
 
+
 def get_elem_param_dict(par_element: dict[str, Element], key: str) -> dict:
     """
-    Obtain a element-wise parametrized quantity for all elements. 
-    Useful for shell-resolved parameters, combines nicely with `IndexHelper`. 
+    Obtain a element-wise parametrized quantity for all elements.
+    Useful for shell-resolved parameters, combines nicely with `IndexHelper`.
 
     Parameters
     ----------
@@ -44,11 +54,10 @@ def get_elem_param_dict(par_element: dict[str, Element], key: str) -> dict:
     Returns
     -------
     Tensor
-        Parametrization of all elements (with 0 index being a dummy to allow indexing by atomic numbers).
+        Parametrization of all elements (starting with 1 to allow indexing by atomic numbers).
     """
 
     d = {}
-
     for i, item in enumerate(par_element.values()):
         d[i + 1] = getattr(item, key)
 
