Fix NaN in SCF gradient

src/xtbml/xtb/h0.py
@@ -303,16 +303,15 @@ class Hamiltonian:
         # ----------------------
         # Eq.24: PI(R_AB, l, l')
         # ----------------------
-        distances = torch.cdist(self.positions, self.positions, p=2).detach()  # FIXME
+        distances = torch.cdist(self.positions, self.positions, p=2)
         rad = self.ihelp.spread_uspecies_to_atom(self.rad)
         rr = torch.where(
-            mask,
+            mask * ~torch.diag_embed(torch.ones_like(real)),
             distances / (rad.unsqueeze(-1) + rad.unsqueeze(-2)),
-            zero,
+            distances.new_tensor(torch.finfo(distances.dtype).eps),
         )
-        rr_sqrt = torch.sqrt(rr)
         rr_sh = self.ihelp.spread_atom_to_shell(
-            rr_sqrt,
+            torch.sqrt(rr),
             (-2, -1),
         )
 

test/test_hamiltonian/test_scf.py
@@ -68,15 +68,14 @@ def test_grad(testcase, dtype=torch.float):
 
     name, ref = testcase
     numbers = samples[name]["numbers"]
-    with torch.autograd.detect_anomaly():
-        positions = samples[name]["positions"].type(dtype).requires_grad_(True)
-        charges = torch.tensor(0.0, dtype=dtype)
+    positions = samples[name]["positions"].type(dtype).requires_grad_(True)
+    charges = torch.tensor(0.0, dtype=dtype)
 
-        calc = Calculator(numbers, positions, par)
+    calc = Calculator(numbers, positions, par)
 
-        results = calc.singlepoint(numbers, positions, charges, verbosity=0)
-        energy = results.get("energy").sum(-1)
+    results = calc.singlepoint(numbers, positions, charges, verbosity=0)
+    energy = results.get("energy").sum(-1)
 
-        energy.backward()
+    energy.backward()
     gradient = positions.grad.clone()
     assert torch.allclose(gradient, ref, atol=tol)