f462cdde3f5e8b277cb5899c5115859986bc1375
marvinfriede <51965259+marvinfriede@users.noreply.github.com>
2022-08-01 09:31:33 +0200

    Remove slow pair filter entirely and refactoring

--- src/xtbml/xtb/h0.py
+++ src/xtbml/xtb/h0.py
@@ -376,130 +376,6 @@ class Hamiltonian:
             Overlap matrix
         """
 
-        # FIXME: the `pair in helper` check is very slow for large lists.
-        def filter_pairs_manually(pairs: Tensor, norbi: int, norbj: int):
-            helper, upairs = [], []
-            for pair in pairs.tolist():
-                if pair in helper:
-                    continue
-
-                # create indices of other entries from subblock
-                k, l = pair[0], pair[1]
-                for ak in range(norbi):
-                    for al in range(norbj):
-                        helper.append([k + ak, l + al])
-
-                upairs.append(pair)
-
-            return torch.tensor(upairs, dtype=torch.long, device=self.device)
-
-        def get_pairs(x: Tensor, i: int) -> Tensor:
-            """Get indices of all unqiue shells pairs with index value `i`.
-
-            Parameters
-            ----------
-            x : Tensor
-                Matrix of unique shell pairs.
-            i : int
-                Value representing all unique shells in the matrix.
-
-            Returns
-            -------
-            Tensor
-                Indices of all unique shells pairs with index value `i` in the matrix.
-            """
-
-            return (x == i).nonzero(as_tuple=False)
-
-        def get_subblock_start(umap: Tensor, i: int, norbi: int, norbj: int) -> Tensor:
-            """
-            Filter out the top-left index of each subblock of unique shell pairs. This makes use of the fact that the pairs are sorted along
-            the rows.
-
-            Example: A "s" and "p" orbital would give the following 4x4 matrix
-            of unique shell pairs:
-            1 2 2 2
-            3 4 4 4
-            3 4 4 4
-            3 4 4 4
-            As the overlap routine gives back tensors of the shape `(norbi, norbj)`,
-            i.e. 1x1, 1x3, 3x1 and 3x3 here, we require only the following four
-            indices from the matrix of unique shell pairs: [0, 0] (1x1), [1, 0]
-            (3x1), [0, 1] (1x3) and [1, 1] (3x3).
-
-
-            Parameters
-            ----------
-            pairs : Tensor
-                Indices of all unique shell pairs of one type (n, 2).
-            norbi : int
-                Number of orbitals per shell.
-            norbj : int
-                Number of orbitals per shell.
-
-            Returns
-            -------
-            Tensor
-                Top-left (i.e. [0, 0]) index of each subblock.
-            """
-
-            # no need to filter out a 1x1 block
-            if norbi == 1 and norbj == 1:
-                return get_pairs(umap, i)
-
-            # sorting along rows allows only selecting every `norbj`th pair
-            if norbi == 1:
-                pairs = get_pairs(umap, i)
-                return pairs[::norbj]
-
-            if norbj == 1:
-                pairs = get_pairs(umap.mT, i)
-
-                # do the same for the transposed pairs, but switch columns
-                # TODO: this may be the same as the above
-                return torch.index_select(pairs[::norbi], 1, torch.tensor([1, 0]))
-
-            # more intricate because we can have variation in two dimensions
-            # TODO: may also work for all other cases, but not yet tested
-            if norbi == norbj:
-                pairs = get_pairs(umap, i)
-
-                # remove every `norbj`th pair as before; only second dim is tricky
-                pairs = pairs[::norbj]
-
-                start = 0
-                rest = pairs
-
-                # init with dummy
-                final = torch.tensor([[-1, -1]])
-
-                while True:
-                    # get number of blocks in a row by counting the number of
-                    # same indices in the first dimension
-                    nb = (pairs[:, 0] == pairs[start, 0]).nonzero().flatten().size(0)
-
-                    # we need to skip the amount of rows in the block
-                    skip = nb * norbi
-
-                    # split for the blocks in each row because there can be different numbers of blocks in a row
-                    target, rest = torch.split(rest, [skip, rest.size(-2) - skip])
-
-                    # select only the top left index of each block
-                    final = torch.cat((final, target[:nb]), 0)
-
-                    start += skip
-                    if start >= pairs.size(-2):
-                        break
-
-                # remove dummy
-                return final[1:]
-
-            ###############################################################
-
-            pairs = get_pairs(umap, i)
-            pairs = pairs[::norbj]
-            return filter_pairs_manually(pairs, norbi, norbj)
-
         def get_overlap(bas: Basis, positions: Tensor, ihelp: IndexHelper) -> Tensor:
             """Overlap calculation for a single molecule.
 
@@ -706,3 +582,106 @@ class Hamiltonian:
             self.par,
             self.ihelp.type(dtype=dtype),
         )
+
+
+# helpers for overlap calculation
+
+
+def get_pairs(x: Tensor, i: int) -> Tensor:
+    """Get indices of all unqiue shells pairs with index value `i`.
+
+    Parameters
+    ----------
+    x : Tensor
+        Matrix of unique shell pairs.
+    i : int
+        Value representing all unique shells in the matrix.
+
+    Returns
+    -------
+    Tensor
+        Indices of all unique shells pairs with index value `i` in the matrix.
+    """
+
+    return (x == i).nonzero(as_tuple=False)
+
+
+def get_subblock_start(umap: Tensor, i: int, norbi: int, norbj: int) -> Tensor:
+    """
+    Filter out the top-left index of each subblock of unique shell pairs. This makes use of the fact that the pairs are sorted along
+    the rows.
+
+    Example: A "s" and "p" orbital would give the following 4x4 matrix
+    of unique shell pairs:
+    1 2 2 2
+    3 4 4 4
+    3 4 4 4
+    3 4 4 4
+    As the overlap routine gives back tensors of the shape `(norbi, norbj)`,
+    i.e. 1x1, 1x3, 3x1 and 3x3 here, we require only the following four
+    indices from the matrix of unique shell pairs: [0, 0] (1x1), [1, 0]
+    (3x1), [0, 1] (1x3) and [1, 1] (3x3).
+
+
+    Parameters
+    ----------
+    pairs : Tensor
+        Indices of all unique shell pairs of one type (n, 2).
+    norbi : int
+        Number of orbitals per shell.
+    norbj : int
+        Number of orbitals per shell.
+
+    Returns
+    -------
+    Tensor
+        Top-left (i.e. [0, 0]) index of each subblock.
+    """
+
+    # no need to filter out a 1x1 block
+    if norbi == 1 and norbj == 1:
+        return get_pairs(umap, i)
+
+    # sorting along rows allows only selecting every `norbj`th pair
+    if norbi == 1:
+        pairs = get_pairs(umap, i)
+        return pairs[::norbj]
+
+    if norbj == 1:
+        pairs = get_pairs(umap.mT, i)
+
+        # do the same for the transposed pairs, but switch columns
+        return torch.index_select(pairs[::norbi], 1, torch.tensor([1, 0]))
+
+    # more intricate because we can have variation in two dimensions
+    pairs = get_pairs(umap, i)
+
+    # remove every `norbj`th pair as before; only second dim is tricky
+    pairs = pairs[::norbj]
+
+    start = 0
+    rest = pairs
+
+    # init with dummy
+    final = torch.tensor([[-1, -1]])
+
+    while True:
+        # get number of blocks in a row by counting the number of
+        # same indices in the first dimension
+        nb = (pairs[:, 0] == pairs[start, 0]).nonzero().flatten().size(0)
+
+        # we need to skip the amount of rows in the block
+        skip = nb * norbi
+
+        # split for the blocks in each row because there can be different numbers of blocks in a row
+        target, rest = torch.split(rest, [skip, rest.size(-2) - skip])
+
+        # select only the top left index of each block
+        final = torch.cat((final, target[:nb]), 0)
+
+        start += skip
+        if start >= pairs.size(-2):
+            break
+
+    # remove dummy
+    return final[1:]
