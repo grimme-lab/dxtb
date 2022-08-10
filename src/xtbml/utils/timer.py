4a2d0675cef1b314107ac2d3427a5471ba4e3a60
marvinfriede <51965259+marvinfriede@users.noreply.github.com>
2022-08-10 21:06:03 +0200

    Better docstrings for timer and allow restarting

--- src/xtbml/utils/timer.py
+++ src/xtbml/utils/timer.py
@@ -1,3 +1,7 @@
+"""
+Definition of a timer class that can contain multiple timers.
+"""
+
 from __future__ import annotations
 import time
 
@@ -16,8 +20,15 @@ class Timers:
             self._start_time = None
             self.elapsed_time = 0.0
 
-        def start(self):
-            """Start a new timer"""
+        def start(self) -> None:
+            """
+            Start a new timer
+
+            Raises
+            ------
+            TimerError
+                If timer is already running.
+            """
 
             if self._start_time is not None:
                 raise TimerError("Timer is running. Use .stop() to stop it")
@@ -25,13 +36,26 @@ class Timers:
             self._start_time = time.perf_counter()
 
         def stop(self) -> float:
-            """Stop the timer, and report the elapsed time"""
+            """
+            Stop the timer.
+
+            Returns
+            -------
+            float
+                Elapsed time in seconds.
+
+            Raises
+            ------
+            TimerError
+                If timer is not running.
+            """
 
             if self._start_time is None:
                 raise TimerError("Timer is not running. Use .start() to start it")
 
-            self.elapsed_time = time.perf_counter() - self._start_time
+            self.elapsed_time += time.perf_counter() - self._start_time
             self._start_time = None
+
             return self.elapsed_time
 
     timers: dict[str, Timer]
@@ -40,19 +64,45 @@ class Timers:
     def __init__(self) -> None:
         self.timers = {}
 
-    def start(self, label) -> None:
-        """Start a new timer"""
+    def start(self, label: str) -> None:
+        """
+        Create a new timer or start an existing timer named `label`.
+
+        Parameters
+        ----------
+        label : str
+            Name of the timer.
+        """
+
+        if label in self.timers:
+            t = self.timers[label]
+            t.start()
+            return
 
         t = self.Timer()
         t.start()
 
-        if label in self.timers:
-            raise TimerError(f"Timer '{label}' does already exist and is running.")
-
         self.timers[label] = t
 
-    def stop(self, label) -> float:
-        """Stop the timer, and report the elapsed time"""
+    def stop(self, label: str) -> float:
+        """
+        Stop the timer
+
+        Parameters
+        ----------
+        label : str
+            Name of the timer.
+
+        Returns
+        -------
+        float
+            Elapsed time in seconds.
+
+        Raises
+        ------
+        TimerError
+            If timer named `label` does not exist.
+        """
 
         if label not in self.timers:
             raise TimerError(f"Timer '{label}' does not exist.")
@@ -63,11 +113,19 @@ class Timers:
         return elapsed_time
 
     def get_times(self) -> dict[str, float]:
-        """Get the elapsed times of all timers"""
+        """
+        Get the elapsed times of all timers,
+
+        Returns
+        -------
+        dict[str, float]
+            Dictionary of timer names and elapsed times.
+        """
 
         return {label: t.elapsed_time for label, t in self.timers.items()}
 
     def print_times(self) -> None:
+        """Print the elapsed times of all timers in a table."""
         d = self.get_times()
         total = d.pop("total")
         s = sum(d.values())
