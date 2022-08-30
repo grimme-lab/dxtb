b1df9c880e089604e7cf3ee38b495709bfd9157c
hoelzerC <hoelzer@thch.uni-bonn.de>
2022-08-30 12:47:29 +0200

    Rename

--- src/xtbml/param/base.py
+++ src/xtbml/param/base.py
@@ -30,7 +30,7 @@ from ..utils.utils import (
     rgetattr,
     rsetattr,
     get_attribute_name_key,
-    get_all_entries,
+    get_all_entries_from_dict,
 )
 
 
@@ -165,7 +165,7 @@ class Param(BaseModel):
         # expand dictionary and lists
         li = []
         for s in str_list:
-            ent = get_all_entries(self, s)
+            ent = get_all_entries_from_dict(self, s)
             if isinstance(ent, list):
                 li.extend(ent)
             else:
