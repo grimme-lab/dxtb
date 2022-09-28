Fix NaN in gradients

src/xtbml/wavefunction/filling.py
@@ -186,7 +186,7 @@ def get_fermi_occupation(
     e_fermi, homo = get_fermi_energy(nel, emo)
     for _ in range(maxiter):
         exponent = (emo - e_fermi) / kt
-        eterm = torch.exp(exponent)
+        eterm = torch.exp(torch.where(exponent < 50, exponent, zero))
 
         # only singly occupied here         v
         fermi = torch.where(exponent < 50, 1.0 / (eterm + 1.0), zero)

test/test_hamiltonian/test_scf.py
@@ -13,7 +13,7 @@ from xtbml.exlibs.tbmalt import batch
 
 from .samples import samples
 
-torch.autograd.set_detect_anomaly(True)
+# torch.autograd.set_detect_anomaly(True)
 opts = {"verbosity": 0, "etemp": 300.0}
 
 