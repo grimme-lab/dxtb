a226e00cd08627f07f0261e760d8e9890a0cb978
marvinfriede <51965259+marvinfriede@users.noreply.github.com>
2022-10-14 23:32:48 +0200

    Move SCF tests to separate directory

--- test/test_hamiltonian/samples.py
+++ test/test_hamiltonian/samples.py
@@ -1,95 +1,21 @@
 """
-Data for testing repulsion taken from https://github.com/grimme-lab/mstore.
+Molecules for testing the Hamiltonian. Reference values are stored in npz file.
 """
 
-import torch
+from ..molecules import mols
 
-from ..molecules import merge_nested_dicts, mols
+from xtbml.typing import Molecule
 
-from xtbml.typing import Molecule, Tensor, TypedDict
-
-
-class Refs(TypedDict):
-    """Format of reference records containing GFN1-xTB and GFN2-xTB reference values."""
-
-    escf: Tensor
-    """SCF energy for GFN1-xTB"""
-
-
-class Record(Molecule, Refs):
-    """Store for molecular information and reference values"""
-
-
-refs: dict[str, Refs] = {
-    "H": {
-        "escf": torch.tensor(-4.0142947446183e-01),
-    },
-    "C": {
-        "escf": torch.tensor(-1.7411359557542),
-    },
-    "Rn": {
-        "escf": torch.tensor(-3.6081562853046),
-    },
-    "H2": {
-        "escf": torch.tensor(-1.0585984032484),
-    },
-    "LiH": {
-        "escf": torch.tensor(-0.88306406116865),
-    },
-    "HLi": {
-        "escf": torch.tensor(0.0),
-    },
-    "HC": {
-        "escf": torch.tensor(0.0),
-    },
-    "HHe": {
-        "escf": torch.tensor(0.0),
-    },
-    "S2": {
-        "escf": torch.tensor(-7.3285116888517),
-    },
-    "H2O": {
-        "escf": torch.tensor(-5.8052489623704e00),
-    },
-    "CH4": {
-        "escf": torch.tensor(-4.3393059719255e00),
-    },
-    "SiH4": {
-        "escf": torch.tensor(-4.0384093532453),
-    },
-    "PbH4-BiH3": {
-        "escf": torch.tensor(-7.6074262079844),
-    },
-    "C6H5I-CH3SH": {
-        "escf": torch.tensor(-27.612142805843),
-    },
-    "MB16_43_01": {
-        "escf": torch.tensor(-33.200116717478),
-    },
-    "LYS_xao": {
-        "escf": torch.tensor(-48.850798066902),
-    },
-    "C60": {
-        "escf": torch.tensor(-128.79148324775),
-    },
-    "vancoh2": {
-        "escf": torch.tensor(-3.2618651888175e02),
-    },
-}
-
-
-extra: dict[str, Record] = {
+extra: dict[str, Molecule] = {
     "H2_nocn": {
         "numbers": mols["H2"]["numbers"],
         "positions": mols["H2"]["positions"],
-        "escf": torch.tensor(0.0),
     },
     "SiH4_nocn": {
         "numbers": mols["SiH4"]["numbers"],
         "positions": mols["SiH4"]["positions"],
-        "escf": torch.tensor(0.0),
     },
 }
 
 
-samples: dict[str, Record] = {**merge_nested_dicts(mols, refs), **extra}
+samples: dict[str, Molecule] = {**mols, **extra}
--- test/test_overlap/samples.py
+++ test/test_overlap/samples.py
@@ -1,5 +1,5 @@
 """
-Molecules for testing overlap. Reference values are stored in npz file.
+Molecules for testing the overlap. Reference values are stored in npz file.
 """
 
 from ..molecules import mols
