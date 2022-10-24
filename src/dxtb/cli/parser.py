972fe931812d966416a05fbdffab71a46b326a66
marvinfriede <51965259+marvinfriede@users.noreply.github.com>
2022-10-24 14:44:38 +0200

    Add defaults and limit choices

--- src/dxtb/cli/parser.py
+++ src/dxtb/cli/parser.py
@@ -5,6 +5,8 @@ Parser for command line options.
 import argparse
 from pathlib import Path
 
+from ..constants import defaults
+
 
 def is_file(path: str | Path) -> str | Path:
     p = Path(path)
@@ -37,7 +39,30 @@ def is_dir(path: str | Path) -> str | Path:
     return path
 
 
-class EtempAction(argparse.Action):
+def action_not_less_than(min_value: float = 0.0):
+    class CustomAction(argparse.Action):
+        """
+        Custom action for limiting possible input values.
+        """
+
+        def __call__(
+            self,
+            parser: argparse.ArgumentParser,
+            args: argparse.Namespace,
+            values: float,
+            option_string: str | None = None,
+        ) -> None:
+            if values < min_value:
+                parser.error(
+                    f"Option '{option_string}' takes only positive values ({values})."
+                )
+
+            setattr(args, self.dest, values)
+
+    return CustomAction
+
+
+class ActionNonNegative(argparse.Action):
     """
     Custom action for limiting possible input values for the electronic temperature.
     """
@@ -112,7 +137,7 @@ class Formatter(argparse.HelpFormatter):
         return argparse.HelpFormatter._split_lines(self, text, width)
 
 
-def argparser(name: str = "dxtb") -> argparse.ArgumentParser:
+def argparser(name: str = "dxtb", **kwargs) -> argparse.ArgumentParser:
     """
     Parses the command line arguments.
 
@@ -122,10 +147,15 @@ def argparser(name: str = "dxtb") -> argparse.ArgumentParser:
         Container for command line arguments.
     """
 
+    desc = kwargs.pop(
+        "description", "dxtb - Fully differentiable extended tight-binding."
+    )
+
     parser = argparse.ArgumentParser(
-        description="dxtb - Fully differentiable extended tight-binding.",
+        description=desc,
         prog=name,
         formatter_class=lambda prog: Formatter(prog, max_help_position=60),
+        **kwargs,
     )
 
     parser.add_argument(
@@ -137,46 +167,63 @@ def argparser(name: str = "dxtb") -> argparse.ArgumentParser:
     parser.add_argument(
         "--chrg",
         type=int,
+        default=defaults.CHARGE,
         help="R|Molecular charge.",
     )
     parser.add_argument(
         "--spin",
         "--uhf",
+        action=action_not_less_than(0.0),
         type=int,
-        default=0,
+        default=defaults.SPIN,
         help="R|Molecular spin.",
     )
     parser.add_argument(
         "--etemp",
-        action=EtempAction,
+        action=action_not_less_than(0.0),
         type=float,
-        default=300.0,
+        default=defaults.ETEMP,
         help="R|Electronic Temperature in K.",
+    )
+    parser.add_argument(
+        "--fermi_maxiter",
+        type=int,
+        default=defaults.FERMI_MAXITER,
+        help="R|Maximum number of iterations for Fermi smearing.",
+    )
+    parser.add_argument(
+        "--fermi_energy_partition",
+        type=str,
+        default=defaults.FERMI_FENERGY_PARTITION,
+        choices=defaults.FERMI_FENERGY_PARTITION_CHOICES,
+        help="R|Partitioning scheme for electronic free energy.",
     )
     parser.add_argument(
         "--maxiter",
         type=int,
-        default=20,
+        default=defaults.MAXITER,
         help="R|Maximum number of SCF iterations.",
     )
     parser.add_argument(
         "-v",
         "--verbosity",
         type=int,
-        default=1,
+        default=defaults.VERBOSITY,
         help="R|Verbosity level of printout.",
     )
     parser.add_argument(
         "--method",
         type=str,
-        default="gfn1",
-        help="R|Method for calculation. (Options: 'gfn1', 'gfn1-xtb')",
+        default=defaults.METHOD,
+        choices=defaults.METHOD_CHOICES,
+        help="R|Method for calculation.",
     )
     parser.add_argument(
         "--guess",
         type=str,
-        default="eeq",
-        help="R|Model for initial charges. (Options: 'sad', 'eeq')",
+        default=defaults.GUESS,
+        choices=defaults.GUESS_CHOICES,
+        help="R|Model for initial charges.",
     )
     parser.add_argument(
         "--grad",
--- src/dxtb/constants/defaults.py
+++ src/dxtb/constants/defaults.py
@@ -4,6 +4,20 @@ Default settings for `dxtb` calculations.
 
 import torch
 
+# General
+
+METHOD = "gfn1"
+"""General method for calculation from the xtb family."""
+
+METHOD_CHOICES = ["gfn1", "gfn1-xtb", "gfn2", "gfn2-xtb"]
+"""List of possible choices for `METHOD`."""
+
+SPIN = 0
+"""Total spin of the system."""
+
+CHARGE = 0
+"""Total charge of the system."""
+
 THRESH = {
     torch.float16: torch.tensor(1e-2, dtype=torch.float16),
     torch.float32: torch.tensor(1e-5, dtype=torch.float32),
@@ -13,8 +27,11 @@ THRESH = {
 
 # SCF settings
 
-GUESS = "eeq"  # "sad"
+GUESS = "eeq"
 """Initial guess for orbital charges."""
+
+GUESS_CHOICES = ["eeq", "sad"]
+"""List of possible choices for `GUESS`."""
 
 MAXITER = 20
 """Maximum number of SCF iterations."""
@@ -24,11 +41,14 @@ VERBOSITY = 1
 
 # Fermi smearing
 
-ETEMP = 300
+ETEMP = 300.0
 """Electronic temperature for Fermi smearing."""
 
 FERMI_MAXITER = 200
 """Maximum number of iterations for Fermi smearing."""
 
-FERMI_FENERGY_PARTITION = "equal"  # "atomic"
+FERMI_FENERGY_PARTITION = "equal"
 """Partitioning scheme for electronic free energy."""
+
+FERMI_FENERGY_PARTITION_CHOICES = ["equal", "atomic"]
+"""List of possible choices for `FERMI_FENERGY_PARTITION`."""
