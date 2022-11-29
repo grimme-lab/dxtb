63522d1adb0be4289f809a29675d663b0b9bae34
Marvin Friede <51965259+marvinfriede@users.noreply.github.com>
2022-11-29 17:37:54 +0100

    Explicit imports in `__init__.py`
    Co-authored-by: hoelzerC <51398942+hoelzerC@users.noreply.github.com>

--- src/dxtb/basis/__init__.py
+++ src/dxtb/basis/__init__.py
@@ -1,4 +1,4 @@
-from .indexhelper import *
+from .indexhelper import IndexHelper
 from .ortho import orthogonalize
 from .slater import to_gauss
 from .type import Basis  # last to avoid circular import
