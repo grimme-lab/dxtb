f62276e86d38df41720dfba6a19b1b11d931e0b4
marvinfriede <51965259+marvinfriede@users.noreply.github.com>
2022-11-30 08:53:40 +0100

    Analytical gradient for CN (single/batch)

--- src/dxtb/ncoord/ncoord.py
+++ src/dxtb/ncoord/ncoord.py
@@ -7,6 +7,7 @@ import torch
 from ..constants import xtb
 from ..data import cov_rad_d3
 from ..typing import CountingFunction, Tensor
+from ..utils import real_pairs
 
 
 def get_coordination_number(
@@ -61,13 +62,7 @@ def get_coordination_number(
             f"with atomic numbers ({numbers.shape})."
         )
 
-    real = numbers != 0
-    mask = (
-        real.unsqueeze(-2)
-        * real.unsqueeze(-1)
-        * ~torch.diag_embed(torch.ones_like(real))
-    )
-
+    mask = real_pairs(numbers, diagonal=True)
     distances = torch.where(
         mask,
         torch.cdist(positions, positions, p=2, compute_mode="use_mm_for_euclid_dist"),
@@ -81,3 +76,53 @@ def get_coordination_number(
         positions.new_tensor(0.0),
     )
     return torch.sum(cf, dim=-1)
+
+
+def get_coordination_number_gradient(
+    numbers: Tensor,
+    positions: Tensor,
+    dcounting_function: CountingFunction,
+    rcov: Tensor | None = None,
+    cutoff: Tensor | None = None,
+    **kwargs,
+) -> Tensor:
+    if cutoff is None:
+        cutoff = positions.new_tensor(xtb.NCOORD_DEFAULT_CUTOFF)
+    if rcov is None:
+        rcov = cov_rad_d3[numbers].type(positions.dtype).to(positions.device)
+    if numbers.shape != rcov.shape:
+        raise ValueError(
+            f"Shape of covalent radii {rcov.shape} is not consistent with "
+            f"({numbers.shape})."
+        )
+    if numbers.shape != positions.shape[:-1]:
+        raise ValueError(
+            f"Shape of positions ({positions.shape[:-1]}) is not consistent "
+            f"with atomic numbers ({numbers.shape})."
+        )
+
+    mask = real_pairs(numbers, diagonal=True)
+    distances = torch.where(
+        mask,
+        torch.cdist(positions, positions, p=2, compute_mode="use_mm_for_euclid_dist"),
+        positions.new_tensor(torch.finfo(positions.dtype).eps),
+    )
+
+    rc = rcov.unsqueeze(-2) + rcov.unsqueeze(-1)
+    dcf = torch.where(
+        mask * (distances <= cutoff),
+        dcounting_function(distances, rc.type(positions.dtype), **kwargs),
+        positions.new_tensor(0.0),
+    )
+
+    # (n_batch, n_atoms, n_atoms, 3)
+    rij = torch.where(
+        mask.unsqueeze(-1),
+        positions.unsqueeze(-2) - positions.unsqueeze(-3),
+        positions.new_tensor(0.0),
+    )
+
+    # (n_batch, n_atoms, n_atoms, 3)
+    dcf = (dcf / distances).unsqueeze(-1) * rij
+
+    return dcf
