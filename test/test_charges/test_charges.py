1eb12bb61b3e0b072e80cbc79fdc0894bd40564d
marvinfriede <51965259+marvinfriede@users.noreply.github.com>
2022-12-21 16:31:26 +0100

    Adjust charge test tolerances

--- test/test_charges/test_charges.py
+++ test/test_charges/test_charges.py
@@ -1,4 +1,19 @@
-"""Testing the charges module."""
+"""
+Testing the charges module
+==========================
+
+This module tests the EEQ charge model including:
+ - single molecule
+ - batched
+ - ghost atoms
+ - autograd via `gradcheck`
+
+Note that `torch.linalg.solve` gives slightly different results (around 1e-5 to 1e-6) across different Python versions for single precision. For double
+precision, however the results are identical.
+"""
+from __future__ import annotations
+
+from math import sqrt
 
 import pytest
 import torch
@@ -12,6 +27,8 @@ from .samples import samples
 
 @pytest.mark.parametrize("dtype", [torch.float, torch.double])
 def test_single(dtype: torch.dtype):
+    tol = sqrt(torch.finfo(dtype).eps) * 10
+
     sample = samples["NH3-dimer"]
     numbers = sample["numbers"]
     positions = sample["positions"].type(dtype)
@@ -24,16 +41,18 @@ def test_single(dtype: torch.dtype):
         dtype=dtype,
     )
     eeq = charges.ChargeModel.param2019().type(dtype)
-
     energy, qat = charges.solve(numbers, positions, total_charge, eeq, cn)
+
     assert qat.dtype == energy.dtype == dtype
-    assert torch.allclose(torch.sum(qat, -1), total_charge, atol=1.0e-7)
-    assert torch.allclose(qat, qref)
-    assert torch.allclose(energy, eref)
+    assert pytest.approx(torch.sum(qat, -1), abs=1e-6) == total_charge
+    assert pytest.approx(qat, abs=tol) == qref
+    assert pytest.approx(energy, abs=tol) == eref
 
 
 @pytest.mark.parametrize("dtype", [torch.float, torch.double])
 def test_ghost(dtype: torch.dtype):
+    tol = sqrt(torch.finfo(dtype).eps)
+
     sample = samples["NH3-dimer"]
     numbers = sample["numbers"].detach().clone()
     numbers[[1, 5, 6, 7]] = 0
@@ -70,16 +89,18 @@ def test_ghost(dtype: torch.dtype):
         dtype=dtype,
     )
     eeq = charges.ChargeModel.param2019().type(dtype)
-
     energy, qat = charges.solve(numbers, positions, total_charge, eeq, cn)
+
     assert qat.dtype == energy.dtype == dtype
-    assert torch.allclose(torch.sum(qat, -1), total_charge, atol=1.0e-7)
-    assert torch.allclose(qat, qref)
-    assert torch.allclose(energy, eref)
+    assert pytest.approx(torch.sum(qat, -1), abs=1e-6) == total_charge
+    assert pytest.approx(qat, abs=tol) == qref
+    assert pytest.approx(energy, abs=tol) == eref
 
 
 @pytest.mark.parametrize("dtype", [torch.float, torch.double])
 def test_batch(dtype: torch.dtype):
+    tol = sqrt(torch.finfo(dtype).eps)
+
     sample1, sample2 = (
         samples["PbH4-BiH3"],
         samples["C6H5I-CH3SH"],
@@ -156,12 +177,12 @@ def test_batch(dtype: torch.dtype):
         dtype=dtype,
     )
     eeq = charges.ChargeModel.param2019().type(dtype)
-
     energy, qat = charges.solve(numbers, positions, total_charge, eeq, cn)
+
     assert qat.dtype == energy.dtype == dtype
-    assert torch.allclose(torch.sum(qat, -1), total_charge, atol=1.0e-7)
-    assert torch.allclose(qat, qref, atol=1.0e-5)
-    assert torch.allclose(energy, eref, atol=1.0e-5)
+    assert pytest.approx(torch.sum(qat, -1), abs=1e-6) == total_charge
+    assert pytest.approx(qat, abs=tol) == qref
+    assert pytest.approx(energy, abs=tol) == eref
 
 
 @pytest.mark.grad
