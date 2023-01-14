a86df5d40c4651622bda6fc7d0cc0597f7fc0f77
marvinfriede <51965259+marvinfriede@users.noreply.github.com>
2023-01-14 10:54:18 +0100

    Clean up

--- src/dxtb/_types.py
+++ src/dxtb/_types.py
@@ -1,58 +1,72 @@
 """
-Type annotations for this project.
+Type annotations
+================
+
+This module contains all type annotations for this project.
+
+Since typing still significantly changes across different Python versions,
+all the special cases are handled here as well.
 """
 # pylint: disable=unused-import
 from __future__ import annotations
 
 import sys
 from pathlib import Path
-from typing import Any, Literal, Optional, Protocol, TypedDict, overload
+from typing import Any, Literal, NoReturn, Protocol, TypedDict, overload
 
 import torch
 from torch import Tensor
 
 from .constants import defaults
 
-# Python 3.11
+# "Self" (since Python 3.11)
 if sys.version_info >= (3, 11):
     from typing import Self
 else:
     from typing_extensions import Self
 
-# Python 3.10
+# "TypeGuard" (since Python 3.10)
 if sys.version_info >= (3, 10):
     from typing import TypeGuard
 else:
     from typing_extensions import TypeGuard
 
-# Python 3.9 + "from __future__ import annotations"
+# starting with Python 3.9, type hinting generics have been moved
+# from the "typing" to the "collections" module
+# (see PEP 585: https://peps.python.org/pep-0585/)
 if sys.version_info >= (3, 9):
-    # starting with Python 3.9, type hinting generics have been moved
-    # from the "typing" to the "collections" module
-    # (see PEP 585: https://peps.python.org/pep-0585/)
     from collections.abc import Callable, Generator, Sequence
-
-    Sliceable = list[Tensor] | tuple[Tensor, ...]
-
-    CountingFunction = Callable[[Tensor, Tensor], Tensor]
-    PathLike = str | Path
-
-    Gather = Callable[[Tensor, int, Tensor], Tensor]
-    Scatter = Callable[[Tensor, int, Tensor, str], Tensor]
-    ScatterOrGather = Gather | Scatter
 else:
-    from typing import Callable, Generator, List, Sequence, Tuple, Union
+    from typing import Callable, Generator, Sequence
 
-    # in Python 3.8, "from __future__ import annotations" only affects
-    # type annotations not type aliases
-    Sliceable = Union[List[Tensor], Tuple[Tensor, ...]]
+# type aliases that do not require "from __future__ import annotations"
+CountingFunction = Callable[[Tensor, Tensor], Tensor]
+Gather = Callable[[Tensor, int, Tensor], Tensor]
+Scatter = Callable[[Tensor, int, Tensor, str], Tensor]
 
-    CountingFunction = Callable[[Tensor, Tensor], Tensor]
+# "from __future__ import annotations" only affects type annotations
+# not type aliases, hence "|" is not allowed before Python 3.10
+if sys.version_info >= (3, 10):
+    Sliceable = list[Tensor] | tuple[Tensor, ...]
+    PathLike = str | Path
+    ScatterOrGather = Gather | Scatter
+elif sys.version_info >= (3, 9):
+    from typing import Union
+
+    Sliceable = Union[list[Tensor], tuple[Tensor, ...]]
     PathLike = Union[str, Path]
-
-    Gather = Callable[[Tensor, int, Tensor], Tensor]
-    Scatter = Callable[[Tensor, int, Tensor, str], Tensor]
     ScatterOrGather = Union[Gather, Scatter]
+elif sys.version_info >= (3, 8):
+    from typing import List, Tuple, Union
+
+    Sliceable = Union[List[Tensor], Tuple[Tensor, ...]]
+    PathLike = Union[str, Path]
+    ScatterOrGather = Union[Gather, Scatter]
+else:
+    raise RuntimeError(
+        f"'dxtb' requires at least Python 3.8 (Python {sys.version_info.major}."
+        f"{sys.version_info.minor}.{sys.version_info.micro} found)."
+    )
 
 
 class Molecule(TypedDict):
@@ -65,7 +79,6 @@ class Molecule(TypedDict):
     """Tensor of 3D coordinates of shape (n, 3)"""
 
 
-# TODO: Extend with type() and to() methods.
 class TensorLike:
     """
     Provide `device` and `dtype` as well as `to()` and `type()` for other
@@ -88,7 +101,7 @@ class TensorLike:
         return self.__device
 
     @device.setter
-    def device(self, *_):
+    def device(self, *_: Any) -> NoReturn:
         """
         Instruct users to use the ".to" method if wanting to change device.
         """
--- src/dxtb/utils/__init__.py
+++ src/dxtb/utils/__init__.py
@@ -5,5 +5,5 @@ Collection of utility functions.
 from .exceptions import *
 from .misc import *
 from .scattergather import *
+from .tensors import *
 from .timing import Timers, timings
-from .torch import *
