b8947a1545726db913055cab80c53e8a8eaeee98
hoelzerC <hoelzer@thch.uni-bonn.de>
2023-02-09 22:44:54 +0100

    Allow for atom-wise and orbital-wise gradient

--- src/dxtb/utils/misc.py
+++ src/dxtb/utils/misc.py
@@ -7,6 +7,8 @@ Collection of utility functions.
 from __future__ import annotations
 
 import torch
+import sys
+import os
 
 from .._types import Any, Tensor, TypeGuard
 from ..constants import ATOMIC_NUMBER
@@ -48,3 +50,15 @@ def is_int_list(x: list[Any]) -> TypeGuard[list[int]]:
 
 def symbol2number(sym_list: list[str]) -> Tensor:
     return torch.flatten(torch.tensor([ATOMIC_NUMBER[s.title()] for s in sym_list]))
+
+
+class HiddenPrints:
+    """Context manager allowing to hide print statements during development."""
+
+    def __enter__(self):
+        self._original_stdout = sys.stdout
+        sys.stdout = open(os.devnull, "w")
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        sys.stdout.close()
+        sys.stdout = self._original_stdout
