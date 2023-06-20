Fix gradient for charges and guess

src/dxtb/charges.py
@@ -100,10 +100,10 @@ class ChargeModel(TensorLike):
         """
 
         return cls(
-            _chi2019,
-            _kcn2019,
-            _eta2019,
-            _rad2019,
+            _chi2019.to(device).type(torch.float if dtype is None else dtype),
+            _kcn2019.to(device).type(torch.float if dtype is None else dtype),
+            _eta2019.to(device).type(torch.float if dtype is None else dtype),
+            _rad2019.to(device).type(torch.float if dtype is None else dtype),
             device=device,
             dtype=dtype,
         )
@@ -272,7 +272,21 @@ def solve(
             "to correctly set the dtype."
         )
 
-    eps = positions.new_tensor(torch.finfo(positions.dtype).eps)
+    eps = torch.tensor(
+        torch.finfo(positions.dtype).eps,
+        device=positions.device,
+        dtype=positions.dtype,
+    )
+    zero = torch.tensor(0.0, device=positions.device, dtype=positions.dtype)
+
+    # square root of two over pi
+    stop = torch.sqrt(
+        torch.tensor(
+            2.0 / math.pi,
+            device=positions.device,
+            dtype=positions.dtype,
+        )
+    )
 
     real = real_atoms(numbers)
     mask = real_pairs(numbers, diagonal=True)
@@ -281,21 +295,23 @@ def solve(
     diagonal = mask.new_zeros(mask.shape)
     diagonal.diagonal(dim1=-2, dim2=-1).fill_(True)
 
-    rhs = torch.concat(
-        (
-            -model.chi[numbers] + torch.sqrt(cn) * model.kcn[numbers],
-            total_charge.unsqueeze(-1),
-        ),
-        dim=-1,
+    cc = torch.where(
+        real,
+        -model.chi[numbers] + torch.sqrt(torch.clamp(cn, min=eps)) * model.kcn[numbers],
+        zero,
     )
+    rhs = torch.concat((cc, total_charge.unsqueeze(-1)), dim=-1)
 
     rad = model.rad[numbers]
-    gamma = 1.0 / torch.sqrt(rad.unsqueeze(-1) ** 2 + rad.unsqueeze(-2) ** 2)
+    rads = torch.clamp(rad.unsqueeze(-1) ** 2 + rad.unsqueeze(-2) ** 2, min=eps)
+    gamma = torch.where(mask, 1.0 / torch.sqrt(rads), zero)
+
     eta = torch.where(
         real,
-        model.eta[numbers] + torch.sqrt(torch.tensor(2.0 / math.pi)) / rad,
+        model.eta[numbers] + stop / rad,
         distances.new_tensor(1.0),
     )
+
     coulomb = torch.where(
         diagonal,
         eta.unsqueeze(-1),
@@ -305,17 +321,18 @@ def solve(
             distances.new_tensor(0.0),
         ),
     )
+
     constraint = torch.where(
         real,
         distances.new_ones(numbers.shape),
         distances.new_zeros(numbers.shape),
     )
-    zero = distances.new_zeros(numbers.shape[:-1])
+    zeros = distances.new_zeros(numbers.shape[:-1])
 
     matrix = torch.concat(
         (
             torch.concat((coulomb, constraint.unsqueeze(-1)), dim=-1),
-            torch.concat((constraint, zero.unsqueeze(-1)), dim=-1).unsqueeze(-2),
+            torch.concat((constraint, zeros.unsqueeze(-1)), dim=-1).unsqueeze(-2),
         ),
         dim=-2,
     )

test/test_charges/test_charges.py
@@ -51,7 +51,7 @@ def test_single(dtype: torch.dtype):
 
 @pytest.mark.parametrize("dtype", [torch.float, torch.double])
 def test_ghost(dtype: torch.dtype):
-    tol = sqrt(torch.finfo(dtype).eps)
+    tol = sqrt(torch.finfo(dtype).eps) * 10
 
     sample = samples["NH3-dimer"]
     numbers = sample["numbers"].clone()
@@ -88,18 +88,18 @@ def test_ghost(dtype: torch.dtype):
         ],
         dtype=dtype,
     )
-    eeq = charges.ChargeModel.param2019().type(dtype)
+    eeq = charges.ChargeModel.param2019(dtype=dtype)
     energy, qat = charges.solve(numbers, positions, total_charge, eeq, cn)
 
     assert qat.dtype == energy.dtype == dtype
     assert pytest.approx(total_charge, abs=1e-6) == torch.sum(qat, -1)
-    assert pytest.approx(qref, abs=tol) == qat
-    assert pytest.approx(eref, abs=tol) == energy
+    assert pytest.approx(qref, abs=1e-6, rel=tol) == qat
+    assert pytest.approx(eref, abs=tol, rel=tol) == energy
 
 
 @pytest.mark.parametrize("dtype", [torch.float, torch.double])
 def test_batch(dtype: torch.dtype):
-    tol = sqrt(torch.finfo(dtype).eps)
+    tol = sqrt(torch.finfo(dtype).eps) * 10
 
     sample1, sample2 = (
         samples["PbH4-BiH3"],

test/test_charges/test_grad.py
@@ -16,37 +16,136 @@ from __future__ import annotations
 
 import pytest
 import torch
+from torch.autograd.gradcheck import gradcheck, gradgradcheck
 
 from dxtb import charges
-from dxtb._types import Tensor
+from dxtb._types import Callable, Tensor
+from dxtb.ncoord import get_coordination_number
+from dxtb.utils import batch
 
 from .samples import samples
 
+sample_list = ["NH3", "NH3-dimer", "PbH4-BiH3", "C6H5I-CH3SH"]
 
-@pytest.mark.grad
-def test_gradcheck(dtype: torch.dtype = torch.double):
-    sample = samples["NH3"]
+tol = 1e-7
+
+
+def gradchecker(
+    dtype: torch.dtype, name: str
+) -> tuple[Callable[[Tensor, Tensor], Tensor], tuple[Tensor, Tensor]]:
+    """Prepare gradient check from `torch.autograd`."""
+    sample = samples[name]
     numbers = sample["numbers"]
     positions = sample["positions"].type(dtype)
     total_charge = sample["total_charge"].type(dtype)
-    cn = torch.tensor(
-        [3.0, 1.0, 1.0, 1.0],
-        dtype=dtype,
-    )
-    eeq = charges.ChargeModel.param2019().type(dtype)
 
+    eeq = charges.ChargeModel.param2019(dtype=dtype)
+
+    # variables to be differentiated
     positions.requires_grad_(True)
     total_charge.requires_grad_(True)
 
-    def func(positions: Tensor, total_charge: Tensor):
-        return torch.sum(
-            charges.solve(numbers, positions, total_charge, eeq, cn)[0], -1
-        )
+    def func(pos: Tensor, tchrg: Tensor):
+        cn = get_coordination_number(numbers, positions)
+        return charges.solve(numbers, pos, tchrg, eeq, cn)[0]
 
-    # pylint: disable=import-outside-toplevel
-    from torch.autograd.gradcheck import gradcheck
+    return func, (positions, total_charge)
 
-    assert gradcheck(func, (positions, total_charge))
 
-    positions.detach_()
-    total_charge.detach()
+@pytest.mark.grad
+@pytest.mark.parametrize("dtype", [torch.double])
+@pytest.mark.parametrize("name", sample_list)
+def test_grad(dtype: torch.dtype, name: str) -> None:
+    """
+    Check a single analytical gradient of positions against numerical
+    gradient from `torch.autograd.gradcheck`.
+    """
+    func, diffvars = gradchecker(dtype, name)
+    assert gradcheck(func, diffvars, atol=tol)
+
+    diffvars[0].detach_()
+    diffvars[1].detach_()
+
+
+@pytest.mark.grad
+@pytest.mark.parametrize("dtype", [torch.double])
+@pytest.mark.parametrize("name", sample_list)
+def test_gradgrad(dtype: torch.dtype, name: str) -> None:
+    """
+    Check a single analytical gradient of positions against numerical
+    gradient from `torch.autograd.gradgradcheck`.
+    """
+    func, diffvars = gradchecker(dtype, name)
+    assert gradgradcheck(func, diffvars, atol=tol)
+
+    diffvars[0].detach_()
+    diffvars[1].detach_()
+
+
+def gradchecker_batch(
+    dtype: torch.dtype, name1: str, name2: str
+) -> tuple[Callable[[Tensor, Tensor], Tensor], tuple[Tensor, Tensor]]:
+    """Prepare gradient check from `torch.autograd`."""
+    sample1, sample2 = samples[name1], samples[name2]
+    numbers = batch.pack(
+        [
+            sample1["numbers"],
+            sample2["numbers"],
+        ]
+    )
+    positions = batch.pack(
+        [
+            sample1["positions"].type(dtype),
+            sample2["positions"].type(dtype),
+        ]
+    )
+    total_charge = batch.pack(
+        [
+            sample1["total_charge"].type(dtype),
+            sample2["total_charge"].type(dtype),
+        ]
+    )
+
+    eeq = charges.ChargeModel.param2019(dtype=dtype)
+
+    # variables to be differentiated
+    positions.requires_grad_(True)
+    total_charge.requires_grad_(True)
+
+    def func(pos: Tensor, tchrg: Tensor):
+        cn = get_coordination_number(numbers, positions)
+        return charges.solve(numbers, pos, tchrg, eeq, cn)[0]
+
+    return func, (positions, total_charge)
+
+
+@pytest.mark.grad
+@pytest.mark.parametrize("dtype", [torch.double])
+@pytest.mark.parametrize("name1", ["NH3"])
+@pytest.mark.parametrize("name2", sample_list)
+def test_grad_batch(dtype: torch.dtype, name1: str, name2: str) -> None:
+    """
+    Check a single analytical gradient of positions against numerical
+    gradient from `torch.autograd.gradcheck`.
+    """
+    func, diffvars = gradchecker_batch(dtype, name1, name2)
+    assert gradcheck(func, diffvars, atol=tol)
+
+    diffvars[0].detach_()
+    diffvars[1].detach_()
+
+
+@pytest.mark.grad
+@pytest.mark.parametrize("dtype", [torch.double])
+@pytest.mark.parametrize("name1", ["NH3"])
+@pytest.mark.parametrize("name2", sample_list)
+def test_gradgrad_batch(dtype: torch.dtype, name1: str, name2: str) -> None:
+    """
+    Check a single analytical gradient of positions against numerical
+    gradient from `torch.autograd.gradgradcheck`.
+    """
+    func, diffvars = gradchecker_batch(dtype, name1, name2)
+    assert gradgradcheck(func, diffvars, atol=tol)
+
+    diffvars[0].detach_()
+    diffvars[1].detach_()