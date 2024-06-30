a30d93babe6b7784cbb694e386db41b79f2e29a8
marvinfriede <51965259+marvinfriede@users.noreply.github.com>
2024-06-30 12:09:22 +0200

    Bump version

--- src/dxtb/__version__.py
+++ src/dxtb/__version__.py
@@ -22,5 +22,5 @@ from tad_mctc._version import __tversion__
 
 __all__ = ["__version__", "__tversion__"]
 
-__version__ = "0.0.0"
+__version__ = "0.0.1"
 """Version of ``dxtb`` in semantic versioning."""
