5806c65e255b10e180a028b99b2694a0e0932819
marvinfriede <51965259+marvinfriede@users.noreply.github.com>
2024-09-24 09:23:31 -0500

    Resolve issue 123

---
+++ examples/issues/123/run.py
@@ -0,0 +1,52 @@
+# This file is part of dxtb.
+#
+# SPDX-Identifier: Apache-2.0
+# Copyright (C) 2024 Grimme Group
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+https://github.com/grimme-lab/dxtb/issues/123
+"""
+from __future__ import annotations
+
+from pathlib import Path
+
+import torch
+from tad_mctc.io import read
+
+import dxtb
+
+
+def main():
+    dd = {"device": torch.device("cpu"), "dtype": torch.double}
+    dxtb.timer.cuda_sync = False
+
+    # read molecule from file
+    f = Path(__file__).parent / "coord"
+    numbers, positions = read.read_from_path(f, **dd)
+    charge = read.read_chrg_from_path(f, **dd)
+
+    opts = {"verbosity": 6, "scp_mode": dxtb.labels.SCP_MODE_CHARGE}
+    calc = dxtb.calculators.GFN1Calculator(numbers, opts=opts, **dd)
+
+    ref = -35.800705002354
+    energy = calc.get_energy(positions, chrg=charge)
+
+    print("\nComparison to xtb:")
+    print(f" - xtb (6.7.1) : {ref: .12f}")
+    print(f" - dxtb (0.1.0): {energy: .12f}")
+    print(f" - diff        : {energy - ref: .12f}")
+
+
+if __name__ == "__main__":
+    main()
