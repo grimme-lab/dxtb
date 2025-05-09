Fix gradients for basis set (#219)

src/dxtb/_src/basis/ortho.py
@@ -92,29 +92,19 @@ def orthogonalize(
         Primitive Gaussian exponents and contraction coefficients for the
         orthonormalized basis function.
     """
-
     coeff_i, coeff_j = coeff
     alpha_i, alpha_j = alpha
-
-    coeff_new = coeff_i.new_zeros(coeff_i.shape[-1] + coeff_j.shape[-1])
-    alpha_new = alpha_i.new_zeros(alpha_i.shape[-1] + alpha_j.shape[-1])
 
     # Calculate overlap between basis functions
     overlap = gaussian_integral(alpha_i, alpha_j, coeff_i, coeff_j)
 
     # Create new basis function from the pair which is orthogonal to the first
     # basis function
-    alpha_new[: alpha_j.shape[-1]], alpha_new[alpha_j.shape[-1] :] = (
-        alpha_j,
-        alpha_i,
-    )
-    coeff_new[: coeff_j.shape[-1]], coeff_new[coeff_j.shape[-1] :] = (
-        coeff_j,
-        -overlap * coeff_i,
-    )
+    alpha_new = torch.cat((alpha_j, alpha_i), dim=-1)
+    coeff_new = torch.cat((coeff_j, -overlap * coeff_i), dim=-1)
 
-    # Normalization of the new basis function might be off, calculate self overlap
+    # Normalization of new basis function might be off, calculate self overlap
     selfoverlap = gaussian_integral(alpha_new, alpha_new, coeff_new, coeff_new)
-    coeff_new /= torch.sqrt(selfoverlap)
+    coeff_new = coeff_new / torch.sqrt(selfoverlap)
 
     return alpha_new, coeff_new

src/dxtb/_src/integral/driver/factory.py
@@ -27,7 +27,7 @@ import torch
 
 from dxtb import IndexHelper
 from dxtb._src.constants import labels
-from dxtb._src.param import Param
+from dxtb._src.param import Param, ParamModule
 from dxtb._src.typing import TYPE_CHECKING, Tensor
 
 from ..base import IntDriver
@@ -46,7 +46,7 @@ __all__ = ["new_driver"]
 def new_driver(
     name: int,
     numbers: Tensor,
-    par: Param,
+    par: Param | ParamModule,
     device: torch.device | None = None,
     dtype: torch.dtype | None = None,
 ) -> IntDriver:
@@ -72,7 +72,7 @@ def new_driver(
 
 def new_driver_libcint(
     numbers: Tensor,
-    par: Param,
+    par: Param | ParamModule,
     device: torch.device | None = None,
     dtype: torch.dtype | None = None,
 ) -> IntDriverLibcint:
@@ -88,7 +88,7 @@ def new_driver_libcint(
 
 def new_driver_pytorch(
     numbers: Tensor,
-    par: Param,
+    par: Param | ParamModule,
     device: torch.device | None = None,
     dtype: torch.dtype | None = None,
 ) -> IntDriverPytorch:
@@ -101,7 +101,7 @@ def new_driver_pytorch(
 
 def new_driver_pytorch_no_analytical(
     numbers: Tensor,
-    par: Param,
+    par: Param | ParamModule,
     device: torch.device | None = None,
     dtype: torch.dtype | None = None,
 ) -> IntDriverPytorchNoAnalytical:
@@ -114,7 +114,7 @@ def new_driver_pytorch_no_analytical(
 
 def new_driver_legacy(
     numbers: Tensor,
-    par: Param,
+    par: Param | ParamModule,
     device: torch.device | None = None,
     dtype: torch.dtype | None = None,
 ) -> IntDriverPytorchLegacy:

test/test_libcint/test_coeff_grad.py
@@ -20,16 +20,18 @@ Test coefficient derivative with libcint.
 
 from __future__ import annotations
 
-from math import sqrt
-
 import pytest
 import torch
-from tad_mctc.autograd import dgradcheck
+from tad_mctc.batch import pack
+from torch.autograd import gradcheck, gradgradcheck
+from torch.nn import Parameter
 
-from dxtb import GFN2_XTB, IndexHelper, ParamModule
+from dxtb import GFN1_XTB, GFN2_XTB, IndexHelper, Param, ParamModule, labels
 from dxtb._src.basis.bas import Basis
 from dxtb._src.exlibs.available import has_libcint
-from dxtb._src.typing import DD, Tensor
+from dxtb._src.integral.driver.factory import new_driver
+from dxtb._src.integral.driver.libcint.driver import IntDriverLibcint
+from dxtb._src.typing import DD, Callable, Tensor
 from dxtb._src.utils import is_basis_list
 
 if has_libcint is True:
@@ -41,18 +43,27 @@ from .samples import samples
 sample_list = ["H2", "LiH", "H2O", "SiH4"]
 
 
-def autograd(name: str, dd: DD, tol: float) -> None:
+def gradchecker(name: str, gfn: Param) -> tuple[
+    Callable[[Tensor], Tensor],  # autograd function
+    Tensor,  # differentiable variables
+]:
+    """Helper function to create a gradient check function."""
+    dd: DD = {"dtype": torch.double, "device": DEVICE}
+
     sample = samples[name]
     numbers = sample["numbers"].to(DEVICE)
     positions = sample["positions"].to(**dd)
 
-    par = ParamModule(GFN2_XTB, **dd)
+    par = ParamModule(gfn, **dd)
 
     slater = par.get("element", "H", "slater")
+    assert isinstance(slater, Parameter)
     slater.requires_grad_(True)
 
     def func(slater: Tensor) -> Tensor:
-        par.get("element", "H", "slater").data = slater
+        p = par.get("element", "H", "slater")
+        assert isinstance(p, Parameter)
+        p.data = slater
 
         ihelp = IndexHelper.from_numbers(numbers, par)
         bas = Basis(numbers, par, ihelp, **dd)
@@ -64,21 +75,107 @@ def autograd(name: str, dd: DD, tol: float) -> None:
         wrapper = libcint.LibcintWrapper(atombases, ihelp)
         return libcint.overlap(wrapper)
 
-    assert dgradcheck(func, slater, atol=tol)
+    return func, slater
 
 
+@pytest.mark.grad
 @pytest.mark.skipif(not has_libcint, reason="libcint not available")
-@pytest.mark.parametrize("dtype", [torch.double])
 @pytest.mark.parametrize("name", sample_list)
-def test_autograd(dtype: torch.dtype, name: str) -> None:
-    dd: DD = {"dtype": dtype, "device": DEVICE}
-    tol = sqrt(torch.finfo(dtype).eps) * 10
-    autograd(name, dd, tol)
+@pytest.mark.parametrize("gfn", [GFN1_XTB, GFN2_XTB])
+def test_gradcheck(name: str, gfn: Param) -> None:
+    """Check single analytical against numerical gradient."""
+    func, diffvars = gradchecker(name, gfn)
+    assert gradcheck(func, diffvars, atol=1e-6)
+
+
+@pytest.mark.grad
+@pytest.mark.skipif(not has_libcint, reason="libcint not available")
+@pytest.mark.parametrize("name", ["MB16_43_01"])
+@pytest.mark.parametrize("gfn", [GFN1_XTB, GFN2_XTB])
+def test_gradcheck_medium(name: str, gfn: Param) -> None:
+    """Check single analytical against numerical gradient."""
+    func, diffvars = gradchecker(name, gfn)
+    assert gradcheck(func, diffvars, atol=1e-6)
+
+
+@pytest.mark.grad
+@pytest.mark.skipif(not has_libcint, reason="libcint not available")
+@pytest.mark.parametrize("name", sample_list)
+@pytest.mark.parametrize("gfn", [GFN1_XTB, GFN2_XTB])
+def test_gradgradcheck(name: str, gfn: Param) -> None:
+    """Check single analytical against numerical gradient."""
+    func, diffvars = gradchecker(name, gfn)
+    assert gradgradcheck(func, diffvars, atol=1e-6)
+
+
+def overlap(driver: IntDriverLibcint) -> Tensor:
+    """Overlap wrapper for single and batched mode."""
+    # batched mode
+    if driver.ihelp.batch_mode > 0:
+        assert isinstance(driver.drv, list)
+        return pack([libcint.overlap(d) for d in driver.drv])
+
+    # single mode
+    assert isinstance(driver.drv, libcint.LibcintWrapper)
+    return libcint.overlap(driver.drv)
+
+
+def gradchecker_batch(name1: str, name2: str, gfn: Param) -> tuple[
+    Callable[[Tensor], Tensor],  # autograd function
+    Tensor,  # differentiable variables
+]:
+    """Helper function to create a gradient check function."""
+    dd: DD = {"dtype": torch.double, "device": DEVICE}
+
+    sample1, sample2 = samples[name1], samples[name2]
+    numbers = pack(
+        (
+            sample1["numbers"].to(DEVICE),
+            sample2["numbers"].to(DEVICE),
+        )
+    )
+    positions = pack(
+        (
+            sample1["positions"].to(**dd),
+            sample2["positions"].to(**dd),
+        )
+    )
+
+    par = ParamModule(gfn, **dd)
+
+    slater = par.get("element", "H", "slater")
+    assert isinstance(slater, Parameter)
+    slater.requires_grad_(True)
+
+    def func(slater: Tensor) -> Tensor:
+        p = par.get("element", "H", "slater")
+        assert isinstance(p, Parameter)
+        p.data = slater
+
+        drv = new_driver(labels.INTDRIVER_LIBCINT, numbers, par)
+        assert isinstance(drv, IntDriverLibcint)
+
+        drv.setup(positions)
+        return overlap(drv)
+
+    return func, slater
 
 
 @pytest.mark.skipif(not has_libcint, reason="libcint not available")
-@pytest.mark.parametrize("dtype", [torch.float, torch.double])
-@pytest.mark.parametrize("name", ["MB16_43_01"])
-def test_autograd_medium(dtype: torch.dtype, name: str) -> None:
-    dd: DD = {"dtype": dtype, "device": DEVICE}
-    autograd(name, dd, 1e-5)
+@pytest.mark.parametrize("name1", ["LiH"])
+@pytest.mark.parametrize("name2", sample_list)
+@pytest.mark.parametrize("gfn", [GFN1_XTB, GFN2_XTB])
+def test_gradcheck_batch(name1: str, name2: str, gfn: Param) -> None:
+    """Check batched analytical against numerical gradient."""
+    func, diffvars = gradchecker_batch(name1, name2, gfn)
+    assert gradcheck(func, diffvars, atol=1e-6)
+
+
+@pytest.mark.skipif(not has_libcint, reason="libcint not available")
+@pytest.mark.parametrize("name1", ["LiH"])
+@pytest.mark.parametrize("name2", sample_list)
+@pytest.mark.parametrize("gfn", [GFN1_XTB, GFN2_XTB])
+def test_gradgradcheck_batch(name1: str, name2: str, gfn: Param) -> None:
+    """Check batched analytical against numerical gradient."""
+    func, diffvars = gradchecker_batch(name1, name2, gfn)
+    assert gradgradcheck(func, diffvars, atol=1e-6)