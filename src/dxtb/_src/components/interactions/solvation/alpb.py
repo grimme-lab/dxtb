Fix gradient errors

src/dxtb/_src/components/interactions/solvation/alpb.py
@@ -230,6 +230,8 @@ class GeneralizedBorn(Interaction):
         dielectric_constant: Tensor,
         alpb: bool = DEFAULT_ALPB,
         kernel: str = DEFAULT_KERNEL,
+        born_scale: float = DEFAULT_BORN_SCALE,
+        born_offset: float = DEFAULT_BORN_OFFSET,
         device: torch.device | None = None,
         dtype: torch.dtype | None = None,
         **kwargs: Any,
@@ -246,7 +248,11 @@ class GeneralizedBorn(Interaction):
 
         if "rvdw" not in kwargs:
             kwargs["rvdw"] = VDW_D3.to(**self.dd)[numbers]
-        self.born_kwargs = kwargs
+
+        self.born_kwargs = {
+            "born_scale": born_scale,
+            "born_offset": born_offset,
+        }
 
     # pylint: disable=unused-argument
     @override

test/test_classical/test_dispersion/test_grad_pos.py
@@ -152,7 +152,7 @@ def gradchecker_batch(dtype: torch.dtype, name1: str, name2: str) -> tuple[
 @pytest.mark.parametrize("name2", slist)
 def test_gradcheck_batch(dtype: torch.dtype, name1: str, name2: str) -> None:
     """
-    Check a single analytical gradient of parameters against numerical
+    Check a batch of analytical gradient of parameters against numerical
     gradient from `torch.autograd.gradcheck`.
     """
     func, diffvars = gradchecker_batch(dtype, name1, name2)
@@ -168,7 +168,7 @@ def test_gradcheck_batch_large(
     dtype: torch.dtype, name1: str, name2: str
 ) -> None:
     """
-    Check a single analytical gradient of parameters against numerical
+    Check a batch of analytical gradient of parameters against numerical
     gradient from `torch.autograd.gradcheck`.
     """
     func, diffvars = gradchecker_batch(dtype, name1, name2)
@@ -183,7 +183,7 @@ def test_gradgradcheck_batch(
     dtype: torch.dtype, name1: str, name2: str
 ) -> None:
     """
-    Check a single analytical gradient of parameters against numerical
+    Check a batch of analytical gradient of parameters against numerical
     gradient from `torch.autograd.gradgradcheck`.
     """
     func, diffvars = gradchecker_batch(dtype, name1, name2)