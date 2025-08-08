6cb1c0d23d2c437126855dab5ff8c0db6f43951f
marvinfriede <51965259+marvinfriede@users.noreply.github.com>
2025-08-08 17:29:24 +0200

    Remove old code

--- src/dxtb/_src/scf/mixer/__init__.py
+++ src/dxtb/_src/scf/mixer/__init__.py
@@ -24,5 +24,4 @@ This module contains the SCF mixers for convergence acceleration.
 from .anderson import Anderson
 from .base import Mixer
 from .broyden import Broyden
-from .diis import DIIS
 from .simple import Simple
